30825cc150936d21aeb12e2db4b27d3b4ef407f2
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2010-04-25 05:08:14 +0000

    Shell Script to create binaries.

---
+++ kloxo/development-create-binaries.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# This file creates binaries.
+# 
+#
+######
+echo "################################"
+echo "### Start compiling"
+# Compile C files
+cd sbin/
+make
+echo "### Finished"
+echo "################################"
+#
