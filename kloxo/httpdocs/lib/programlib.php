1d3bf1fcdbda729cf546fc1393182c655bea32d2
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2010-05-28 20:36:10 +0000

    * ouch unzip -o should not asking about overwrite but it is... unzip bug?

--- kloxo/httpdocs/lib/programlib.php
+++ kloxo/httpdocs/lib/programlib.php
@@ -39,11 +39,11 @@ function installapp_data_update()
 //      lxfile_mkdir("__path_kloxo_httpd_root/installappdata");
         lxfile_rm_rec("/home/kloxo/httpd/installappdata");
         lxfile_mkdir("/home/kloxo/httpd/installapp");
-	lxfile_mkdir("/home/kloxo/httpd/installappdata");
+//	lxfile_mkdir("/home/kloxo/httpd/installappdata");
         print(" OK\n");
         print(fill_string("Unpack new InstallApp data",50));
 //      lxshell_unzip("lxlabs", "__path_kloxo_httpd_root/installappdata/", "/tmp/installappdata.zip");
-        system("cd /home/kloxo/httpd/ ; unzip -oqq /tmp/installappdata.zip");
+        system("cd /home/kloxo/httpd/ ; unzip -qq /tmp/installappdata.zip");
         print(" OK\n");
  	print(fill_string("Remove downloaded InstallApp data zip file", 50));
 	lxfile_rm("/tmp/installappdata.zip");
