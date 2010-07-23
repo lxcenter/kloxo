3ae9bdd1a236d86c3b6b0e4a250c39c5884dc34b
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2010-07-23 22:52:56 +0000

    * Add logging when scavenge is running.

--- kloxo/bin/scavenge.php
+++ kloxo/bin/scavenge.php
@@ -1,43 +1,70 @@
-<?php 
+<?php
+//
+//    Kloxo, Hosting Panel
+//
+//    Copyright (C) 2000-2009     LxLabs
+//    Copyright (C) 2009-2010     LxCenter
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  This is the scavenge file. It is a master cronjob that runs once every 24h (default at 3:57 AM)
+//
+
 include_once "htmllib/lib/displayinclude.php";
 
 exit_if_secondary_master();
 exit_if_another_instance_running();
 scavenge_main();
 
-function scavenge_main()
-{
-	global $gbl, $sgbl, $login, $ghtml; 
+function scavenge_main() {
+    global $gbl, $sgbl, $login, $ghtml;
+    log_shell("Scavenge: Start")
+    initProgramlib('admin');
+    log_shell("Scavenge: Collect Traffic");
+    passthru("$sgbl->__path_php_path ../bin/gettraffic.php");
+    log_shell("Scavenge: Collect Quota");
+    passthru("$sgbl->__path_php_path ../bin/collectquota.php");
+    log_shell("Scavenge: Schedule backups");
+    passthru("$sgbl->__path_php_path ../bin/common/schedulebackup.php");
+    log_shell("Scavenge: Clear Sessions");
+    passthru("$sgbl->__path_php_path ../bin/common/clearsession.php");
+    log_shell("Scavenge: Self backup");
+    passthru("$sgbl->__path_php_path ../bin/common/mebackup.php");
+    log_shell("Scavenge: Check Cluster Disk Quota");
+    checkClusterDiskQuota();
 
-	passthru("$sgbl->__path_php_path ../bin/gettraffic.php");
-	passthru("$sgbl->__path_php_path ../bin/collectquota.php");
-	passthru("$sgbl->__path_php_path ../bin/common/schedulebackup.php");
-	passthru("$sgbl->__path_php_path ../bin/common/clearsession.php");
-	passthru("$sgbl->__path_php_path ../bin/common/mebackup.php");
-	//passthru("$sgbl->__path_php_path htmllib/lbin/getlicense.php");
+    $driverapp = $gbl->getSyncClass(null, 'localhost', 'web');
+    if ($driverapp === 'lighttpd') {
+        log_shell("Scavenge: Restarting lighttpd");
+        system("service lighttpd restart");
+    }
 
-	initProgramlib('admin');
+    log_shell("Scavenge: Fix log dir");
+    passthru("$sgbl->__path_php_path ../bin/common/fixlogdir.php");
+    log_shell("Scavenge: InstallApp update");
+    passthru("$sgbl->__path_php_path ../bin/installapp-update.phps");
 
-	
-	checkClusterDiskQuota();
+    log_shell("Scavenge: Watchdog checks");
+    $rs = get_all_pserver();
+    foreach ($rs as $r) {
+        watchdog::addDefaultWatchdog($r);
+    }
 
-	$driverapp = $gbl->getSyncClass(null, 'localhost', 'web');
-
-	if ($driverapp === 'lighttpd') {
-		system("service lighttpd restart");
-	}
-	passthru("$sgbl->__path_php_path ../bin/common/fixlogdir.php");
-
-	passthru("$sgbl->__path_php_path ../bin/installapp-update.phps");
-
-	$rs = get_all_pserver();
-	foreach($rs as $r) {
-		watchdog::addDefaultWatchdog($r);
-	}
-	lxguard::collect_lxguard();
-	fix_all_mysql_root_password();
-
-	auto_update();
-
-
+    log_shell("Scavenge: Collect LxGuard info");
+    lxguard::collect_lxguard();
+    log_shell("Scavenge: Fix MySQL root password");
+    fix_all_mysql_root_password();
+    log_shell("Scavenge: Auto update Kloxo");
+    auto_update();
 }
--- kloxo/httpdocs/htmllib/lib/displayinclude.php
+++ kloxo/httpdocs/htmllib/lib/displayinclude.php
@@ -1,13 +1,25 @@
 <?php 
-
+//
+//    Kloxo, Hosting Panel
+//
+//    Copyright (C) 2000-2009     LxLabs
+//    Copyright (C) 2009-2010     LxCenter
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 $path = __FILE__;
 $dir = dirname(dirname(dirname($path)));
-
 include_once "$dir/htmllib/lib/includecore.php";
-
 include_once "lib/html.php";
-include_once "htmllib/lib/include.php"; 
-
-
+include_once "htmllib/lib/include.php";
 $ghtml = new Html();
-
