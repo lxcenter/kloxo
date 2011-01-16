230b5584a9c6e132665cc3bd68133052c5f8d6e9
martin.sefcik <martin.sefcik@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-01-16 01:38:34 +0000

    Code cleanup and reformat.

--- kloxo/httpdocs/htmllib/lib/traffichistorylib.php
+++ kloxo/httpdocs/htmllib/lib/traffichistorylib.php
@@ -1,150 +1,141 @@
 <?php 
 
-class TrafficHistory extends lxclass {
-
-static $__desc =  Array("", "",  "traffic_history");
-static $__desc_nname =  Array("", "",  "device_name");
-static $__desc_parent_name =  Array("", "",  "device_name");
-static $__desc_month    =   Array("", "",  "month");
-static $__desc_traffic_usage     =  Array("", "",  "total_traffic_(MB)");
-static $__acdesc_list     =  Array("", "",  "traffic_history");
-
-//Objects
-
-//Lists
-
-function write() {}
-function get() {}
-
-
-function isSelect()
+class TrafficHistory extends Lxclass
 {
-	return false;
-}
+	static $__desc               = array('', '', 'traffic_history');
+	static $__desc_nname         = array('', '', 'device_name');
+	static $__desc_parent_name   = array('', '', 'device_name');
+	static $__desc_month         = array('', '', 'month');
+	static $__desc_traffic_usage = array('', '', 'total_traffic_(MB)');
+	static $__acdesc_list        = array('', '', 'traffic_history');
 
-static function createListAlist($parent, $class)
-{
-	$alist[] = "a=list&c=$class";
-	return $alist;
-}
+	function write() {}
+	function get() {}
 
-static function createListBlist($parent, $class)
-{
-	return null;
-}
-
-static function defaultSort()
-{
-	return "nname";
-}
-
-static function defaultSortDir()
-{
-	return 'desc';
-}
-
-function display($var)
-{
-	if ($var == 'month') {
-		$month = date('n', $this->$var);
-		$month = intToMonth($month);
-		$year = date('Y', $this->$var);
-		return "$year $month";
+	function isSelect()
+	{
+		return false;
 	}
 
-	return parent::display($var);
-}
-
-function getTrafficRealObject()
-{
-	return strtil($this->get__table(), "traffic");
-}
-
-function getAllTrafficMonthly() 
-{
-
-	global $gbl, $sgbl, $login, $ghtml; 
-
-	$robjname = $this->getTrafficRealObject();
-	$login->loadAllObjects($robjname);
-
-	$domlist = $login->getList($robjname);
-
-	foreach($objlist as $obj) {
-		$trafficlist[$obj->nname] = self::getTrafficMonthly($obj);
+	static function createListAlist($parent, $class)
+	{
+		$alist[] = "a=list&c=$class";
+		return $alist;
 	}
-	
-	return $trafficlist;
-}
 
-static function getTrafficMonthly($object, $trafficname, $extra_var)  
-{
-	
-	$tobjectlist  = $object->getList($trafficname);
-
-	if (!$tobjectlist) {
+	static function createListBlist($parent, $class)
+	{
 		return null;
 	}
-	$list1 = get_namelist_from_objectlist($tobjectlist);
-	$list = lx_array_keys($list1);
-	list(, $start, ) = explode( ':', $list[0]); 
-	$count = count($list);
-	list( , , $end ) = explode(':', $list[$count-1]);
-	$smonth = @ strftime("%m", $start);
-	$emonth = @ strftime("%m", $end);
-	$name = $object->nname;
 
-	$month = @ date("n");
-	$year = @ date("Y");
-	$count = 0;
-	while ($count < 13) {
-		$totallist[] = self::getMonthTotal($tobjectlist, $month, $year, $extra_var);
-		if ($month == 1) {
-			$month = 12;
-			$year--;
-		} else {
-			$month--;
-		}
-		$count++;
+	static function defaultSort()
+	{
+		return 'month';
 	}
 
-	return $totallist;
-}
-
-static function getMonthTotal($list, $month, $year, $extra_var) 
-{
-
-	$tot = 0;
-	foreach((array) $extra_var as $v) {
-		$res[$v] = 0;
+	static function defaultSortDir()
+	{
+		return 'desc';
 	}
-	$nname = '1110';
-	foreach((array) $list as $t ) {
-		list($domname, $oldtime, $newtime) = explode( ":", $t->nname);
-		$cmonth = @ strftime("%m" , $oldtime);
-		//dprint(strftime("%c" , "$oldtime"). ": "); dprint($t->traffic_usage); dprint("$cmonth <br> \n");
-		//		dprint("$cmonth $month $t->traffic_usage <br>");
-		$yy = @ date("Y", $oldtime);
-		if ($yy != $year) {
-			continue;
+
+	function display($var)
+	{
+		if ($var == 'month') {
+			$month = date('n', $this->$var);
+			$month = intToMonth($month);
+			$year = date('Y', $this->$var);
+			return "$year $month";
 		}
 
-		if($cmonth == $month) {
-			$tot +=  $t->traffic_usage;
-			foreach((array) $extra_var as $v) {
-				$res[$v] += $t->$v;
+		return parent::display($var);
+	}
+
+	function getTrafficRealObject()
+	{
+		return strtil($this->get__table(), 'traffic');
+	}
+
+	function getAllTrafficMonthly()
+	{
+		global $login;
+
+		$robjname = $this->getTrafficRealObject();
+		$login->loadAllObjects($robjname);
+
+		$domlist = $login->getList($robjname);
+
+		foreach($domlist as $obj) {
+			$trafficlist[$obj->nname] = self::getTrafficMonthly($obj);
+		}
+
+		return $trafficlist;
+	}
+
+	static function getTrafficMonthly($object, $trafficname, $extra_var)
+	{
+		$tobjectlist  = $object->getList($trafficname);
+
+		if (!$tobjectlist) {
+			return null;
+		}
+		$list1 = get_namelist_from_objectlist($tobjectlist);
+		$list = lx_array_keys($list1);
+		list(, $start, ) = explode( ':', $list[0]);
+		$count = count($list);
+		list( , , $end ) = explode(':', $list[$count-1]);
+		$smonth = @ strftime("%m", $start);
+		$emonth = @ strftime("%m", $end);
+		$name = $object->nname;
+
+		$month = @ date("n");
+		$year = @ date("Y");
+		$count = 0;
+		while ($count < 13) {
+			$totallist[] = self::getMonthTotal($tobjectlist, $month, $year, $extra_var);
+			if ($month == 1) {
+				$month = 12;
+				$year--;
+			} else {
+				$month--;
 			}
-			$nname = $oldtime;
+			$count++;
 		}
+
+		return $totallist;
 	}
 
-	$timestamp = mktime(0, 0, 0, $month, 1, $year);
-	$res['nname'] = $timestamp;
-	$res['month'] = $timestamp;
-	$res['traffic_usage'] = $tot;
+	static function getMonthTotal($list, $month, $year, $extra_var)
+	{
+		$tot = 0;
+		foreach((array) $extra_var as $v) {
+			$res[$v] = 0;
+		}
+		$nname = '1110';
+		foreach((array) $list as $t ) {
+			list($domname, $oldtime, $newtime) = explode( ":", $t->nname);
+			$cmonth = @ strftime("%m" , $oldtime);
+			$yy = @ date("Y", $oldtime);
+			if ($yy != $year) {
+				continue;
+			}
 
-	return $res;
-	 
-}
+			if($cmonth == $month) {
+				$tot +=  $t->traffic_usage;
+				foreach((array) $extra_var as $v) {
+					$res[$v] += $t->$v;
+				}
+				$nname = $oldtime;
+			}
+		}
+
+		$timestamp = mktime(0, 0, 0, $month, 1, $year);
+		$month = intToMonth($month);
+		
+		$res['nname'] = "$year $month";
+		$res['month'] = $timestamp;
+		$res['traffic_usage'] = $tot;
+
+		return $res;
+	}
 
 }
--- kloxo/httpdocs/lib/domain/domaintraffichistorylib.php
+++ kloxo/httpdocs/lib/domain/domaintraffichistorylib.php
@@ -1,36 +1,35 @@
 <?php
 
-class DomaintrafficHistory extends TrafficHistory {
-
-//Core
-
-static $__desc_ftptraffic_usage     =  Array("", "",  "ftp_traffic_(MB)");
-static $__desc_webtraffic_usage     =  Array("", "",  "web_(MB)");
-static $__desc_mailtraffic_usage     =  Array("", "",  "mail_(MB)");
-//Data
-
-static function createListNlist($parent, $view)
+class DomaintrafficHistory extends TrafficHistory
 {
-	//$nlist['nname'] = '100%';
-	$nlist['month'] = '100%';
-	$nlist['ftptraffic_usage'] = '50%';
-	$nlist['mailtraffic_usage'] = '50%';
-	$nlist['webtraffic_usage'] = '50%';
-	$nlist['traffic_usage'] = '50%';
-	return $nlist;
-}
+	static $__desc_ftptraffic_usage  = array('', '', 'ftp_traffic_(MB)');
+	static $__desc_webtraffic_usage  = array('', '', 'web_(MB)');
+	static $__desc_mailtraffic_usage = array('', '', 'mail_(MB)');
 
-function isSync() { return false; }
+	static function createListNlist($parent, $view)
+	{
+		$nlist['month']             = '100%';
+		$nlist['ftptraffic_usage']  = '50%';
+		$nlist['mailtraffic_usage'] = '50%';
+		$nlist['webtraffic_usage']  = '50%';
+		$nlist['traffic_usage']     = '50%';
+		return $nlist;
+	}
 
-static function getExtraVar()
-{
-	return array('ftptraffic_usage', 'webtraffic_usage', 'mailtraffic_usage');
-}
+	function isSync()
+	{
+		return false;
+	}
 
-static function initThisList($parent, $class)
-{
-	$result =  self::getTrafficMonthly($parent, 'domaintraffic', self::getExtraVar());
-	return $result;
-}
+	static function initThisList($parent, $class)
+	{
+		$result = self::getTrafficMonthly($parent, 'domaintraffic', self::getExtraVar());
+		return $result;
+	}
+
+	static function getExtraVar()
+	{
+		return array('ftptraffic_usage', 'webtraffic_usage', 'mailtraffic_usage');
+	}
 
 }
