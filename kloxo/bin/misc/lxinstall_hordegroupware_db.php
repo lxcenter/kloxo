63b7e273e49b89321bcf36db91dad9bc4f2748f8
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-01-29 00:48:29 +0000

    Fix for HordeDatabase (/script/fixwebmail). Cript fixwebmail never ever worked in all LxAdmin/Kloxo versions. Still to add is RoundCube function. Related to issue #421

--- kloxo/bin/misc/lxinstall_hordegroupware_db.php
+++ kloxo/bin/misc/lxinstall_hordegroupware_db.php
@@ -1,9 +1,20 @@
 <?php
+//
+//
+// Beta Version 29-jan-2011
+//
+//
+//
+
+print("Start fixing webmail\n\n");
+
+print("Fixing Horde......\n###\n");
+
 include_once "htmllib/lib/include.php"; 
+
 $pass = slave_get_db_pass();
 
 debug_for_backend();
-
 
 $user = "root";
 $host = "localhost";
@@ -14,45 +25,94 @@ if (!$link) {
 	print("Mysql root password error\n");
 	exit;
 }
+print("Connected to MySQL\nTry to create the horde database...\n");
 
-dprint("Granting all privileges\n");
-mysql_query("create database horde_groupware");
-$pass = randomString(8);
-$content = lfile_get_contents("../file/horde.config.phps");
-$content = str_replace("__lx_horde_pass", $pass, $content);
-lfile_put_contents("/home/kloxo/httpd/webmail/horde/config/conf.php", $content);
-mysql_query("grant all on horde_groupware.* to horde_groupware@localhost identified by '$pass'", $link);
-mysql_query("flush privileges", $link);
-		
-horde_check_database($link, $database, $pass);
+$result = mysql_query("CREATE DATABASE `horde_groupware`");
 
-	
-function horde_check_database($link, $database, $pass) 
+if (!$result)
 {
-	$found = false;
-	$result = mysql_query('SHOW DATABASES;', $link);
-	$pass = slave_get_db_pass();
+ print("Database already exists. Check for tables\n");
+ $result = mysql_select_db('horde_groupware');
+ if (!$result) { print("Something went wrong, can not select horde database!\n"); }
 
-	/*
-	while( $data = mysql_fetch_row($result) ) {
-		if(!strcmp($data[0],$database)) {
-			print(" Database already exists\n");
-			echo "exiting.........\n" ;
-			$found = true;
-			break;
-		}
-	}
-*/
+ $query = "SELECT `user_uid` FROM `horde_users` WHERE `user_uid`='admin'"; 
+ $result = mysql_query($query);
+ if (!$result) {
+  print("Something went wrong, could not find admin user\n");
+ } else {
+  print("Your Database looks fine. No need to fix\n");
+  exit;
+ }
 
-	dprint("We did not find our database $database\n");
-	dprint("Creating a database $database\n");
-	$pstring = null;
-	if ($pass) { $pstring = "-p\"$pass\""; }
-	system("mysql -u horde_groupware $pstring < /home/kloxo/httpd/webmail/horde/scripts/sql/groupware.mysql.sql 2>/dev/null");
-	system("mysql -u horde_groupware $pstring horde_groupware < /home/kloxo/httpd/webmail/horde/turba/scripts/sql/turba.mysql.sql 2>/dev/null");
+ print("Dropping database\n");
+ $result = mysql_query("DROP DATABASE `horde_groupware`");
+ if (!$result)
+ {
+  print("Could not drop horde database!\nScript Abort\n\n");
+  exit;
+ }
 
-	@ mysql_select_db('horde_groupware');
-	$query = "INSERT INTO horde_users (user_uid, user_pass) VALUES ('admin','21232f297a57a5a743894a0e4a801fc3')" ;
-	$result = @ mysql_query($query);
+ print("Create database\n");
+ $result = mysql_query("CREATE DATABASE `horde_groupware`");
+ if (!$result) {
+  print("There is REALY something very very wrong... Go to http://forum.lxcenter.org/ and report.\n\n");
+  exit;
+ }
 
 }
+
+print("Generating password..\n");
+$pass = randomString(8);
+dprint("Generated Pass ".$pass."\n");
+print("Add Password to configfile\n");
+$content = lfile_get_contents("../file/horde.config.phps");
+$content = str_replace("__lx_horde_pass", $pass, $content);
+print("Remove system readonly attribute from configfile\n");
+system("chattr -i /home/kloxo/httpd/webmail/horde/config/conf.php");
+
+lfile_put_contents("/home/kloxo/httpd/webmail/horde/config/conf.php", $content);
+
+print("Granting privileges\n");
+$result = mysql_query("GRANT ALL ON horde_groupware.* TO horde_groupware@localhost IDENTIFIED BY '$pass'", $link);
+mysql_query("flush privileges", $link);
+if (!$result) { print("Could not grant privileges\nScript Abort.\n"); exit; }
+	
+print("Fix database values in horde sql importfile\n");
+
+$hordefile = "/home/kloxo/httpd/webmail/horde/scripts/sql/groupware.mysql.sql";
+$content = lfile_get_contents($hordefile);
+$content = str_replace("CREATE DATABASE horde;", "CREATE DATABASE IF NOT EXISTS horde_groupware;", $content);
+lfile_put_contents($hordefile, $content);
+
+$content = lfile_get_contents($hordefile);
+$content = str_replace("USE horde;", "USE horde_groupware;", $content);
+lfile_put_contents($hordefile, $content);
+
+$content = lfile_get_contents($hordefile);
+$content = str_replace(") ENGINE = InnoDB;", ");", $content);
+lfile_put_contents($hordefile, $content);
+
+
+	$pass = slave_get_db_pass();
+
+	$pstring = null;
+	if ($pass) { $pstring = "-p\"$pass\""; }
+        print("Importing Horde database structure\n");
+	system("mysql -u root $pstring < /home/kloxo/httpd/webmail/horde/scripts/sql/groupware.mysql.sql");
+
+	$result = mysql_select_db('horde_groupware');
+        if (!$result) { print("Something went wrong, can not select horde database!\n"); }
+//
+// TODO: Is this user realy needed!?!?
+//
+	$query = "INSERT INTO horde_users (user_uid, user_pass) VALUES ('admin','21232f297a57a5a743894a0e4a801fc3')" ;
+	$result = mysql_query($query);
+        if (!$result) { print("Something went wrong, could not add admin user into horde database\n"); }
+
+
+print("###\nFixing RoundCube\n");
+print("Sorry, roundcubefix is not created yet....\n\n");
+
+
+print("Done\n");
+exit;
