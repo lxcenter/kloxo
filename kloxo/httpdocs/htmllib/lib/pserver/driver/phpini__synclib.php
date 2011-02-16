4ce3b4b34c4f3a8006d17a20f19bcd007fd4ad5a
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-02-16 23:57:27 +0000

    Small cleanup and error trap for custom.php.ini #435

--- kloxo/httpdocs/htmllib/lib/pserver/driver/phpini__synclib.php
+++ kloxo/httpdocs/htmllib/lib/pserver/driver/phpini__synclib.php
@@ -11,7 +11,6 @@ function initString($ver)
 
 if ($this->main->phpini_flag_b->isON('enable_zend_flag')) {
 	$this->main->phpini_flag_b->enable_zend_val =<<<XML
-
 [Zend]
 zend_extension_manager.optimizer=/usr/lib/kloxophp/zend/lib/Optimizer-3.2.8
 zend_extension_manager.optimizer_ts=/usr/lib/kloxophp/zend/lib/Optimizer_TS-3.2.8
@@ -33,7 +32,6 @@ XML;
 	lxfile_rm("../etc/flag/xcache_enabled.flg");
 	$this->main->phpini_flag_b->enable_xcache_val = "";
 }
-
 
 if ($this->main->phpini_flag_b->isON('enable_ioncube_flag')) {
 	$this->main->phpini_flag_b->enable_ioncube_val =<<<XML
@@ -60,13 +58,9 @@ function createIniFile()
 {
 	$pclass = $this->main->getParentClass();
 
-
 	$ver = find_php_version();
 
-	//dprintr($this->main->phpini_flag_b);
 	$this->initString($ver);
-
-	//dprintr($this->main->phpini_flag_b);
 
 	$header = lfile_get_contents("../file/phpini/php.ini.template-$ver");
 	$cont = lfile_get_contents("../file/phpini/php.ini.temp");
@@ -92,45 +86,6 @@ function createIniFile()
 		list($cont) = $this->replacestr(array($cont), $vl);
 	}
 
-
-	/*
-	list($cont) = $this->replacestr(array($cont), 'enable_zend_val');
-	list($cont) = $this->replacestr(array($cont), 'enable_ioncube_val');
-	list($cont) = $this->replacestr(array($cont), 'enable_xcache_val');
-
-
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'register_global_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'output_compression_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'display_error_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'file_uploads_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'upload_max_filesize');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'log_errors_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'upload_tmp_dir_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'output_buffering_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'register_argc_argv_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'magic_quotes_gpc_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'variables_order_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'magic_quotes_runtime_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'magic_quotes_sybase_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'gpc_order_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'extension_dir_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'enable_dl_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'mysql_allow_persistent_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'disable_functions_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'max_execution_time_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'max_input_time_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'memory_limit_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'post_max_size_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'register_long_arrays_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'allow_url_fopen_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'allow_url_include_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'session_save_path_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'cgi_force_redirect_flag');
-	list($cont, $htcont) = $this->replacestr(array($cont, $htcont), 'safe_mode_flag');
-	*/
-
-
-
 	$stlist[] = "###Start Kloxo PHP config Area";
 	$stlist[] = "###Start Lxdmin Area";
 	$stlist[] = "###Start Kloxo Area";
@@ -143,7 +98,6 @@ function createIniFile()
 	$endstring = $endlist[0];
 	$startstring = $stlist[0];
 
-
 	if ($pclass === 'pserver') {
 		$file = "/etc/php.ini";
 		if (!lxfile_exists("__path_kloxo_back_phpini")) {
@@ -152,15 +106,23 @@ function createIniFile()
 		$this->enableDisableModule("enable_xcache_flag", "xcache");
 		$htfile = null;
 		$extrafile = "/etc/custom.php.ini";
-		$extrastring = lfile_get_contents("/etc/custom.php.ini");
+                if (lxfile_exists($extrafile)) {
+		$extrastring = lfile_get_contents($extrafile);
 		$cont = "$extrastring\n$cont";
+		} else {
+		$cont = "$cont";
+		}
 	} else {
 		$dname = $this->main->getParentName();
 		$elogfile = "/home/{$this->main->__var_customer_name}/__processed_stats/{$this->main->getParentName()}.phplog";
 		$file = "__path_httpd_root/{$this->main->getParentName()}/php.ini";
 		$extrafile = "__path_httpd_root/{$this->main->getParentName()}/custom.php.ini";
+		if (lxfile_exists($extrafile)) {
 		$extrastring = lfile_get_contents($extrafile);
-		$htfile = "{$this->main->__var_docrootpath}/.htaccess";
+		} else {
+		$extrastring = "";
+		}
+		$htfile = "{$this->main->__var_docrootpath}/.htaccess";		
 		$ht1file = "/home/httpd/{$this->main->getParentName()}/kloxoscript/.htaccess";
 		$htcont = "php_value error_log $elogfile\n$htcont";
 		$htcont = "<Ifmodule mod_php4.c>\n$htcont\n</Ifmodule>\n<Ifmodule mod_php5.c>\n$htcont\n</Ifmodule>\n";
@@ -175,7 +137,6 @@ function createIniFile()
 		$cont = "error_log = $elogfile\n$cont";
 		$cont = "$extrastring\n$cont";
 	}
-
 	
 	lxfile_rm($file);
 	lfile_put_contents($file, "$header\n$cont\n");
