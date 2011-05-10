47f3bebb9c95144b2ef9cdc57e15d88d7bad3448
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-05-10 06:18:01 +0000

    Issue #440 - Logging for /script/fixmail

--- kloxo/bin/fix/fixmmail.php
+++ kloxo/bin/fix/fixmmail.php
@@ -3,6 +3,12 @@
 include_once "htmllib/lib/include.php"; 
 initProgram('admin');
 
+print("###################################\n");
+print("Usage: /script/fixmail [--server=servername]\n");
+print("###################################\n");
+print("Running script fixmail...\n");
+
+
 $list = parse_opt($argv);
 
 if (isset($list['server'])) {
@@ -10,24 +16,27 @@ if (isset($list['server'])) {
 } else {
 	$server = 'localhost';
 }
+print("Server: " . $server . "\n");
 
 
 
 $login->loadAllObjects('client');
 $list = $login->getList('client');
 
+
 foreach($list as $c) {
 	$dlist = $c->getList('domaina');
 	foreach((array)$dlist as $l) {
 		$mmail = $l->getObject('mmail');
 		if ($mmail->syncserver !== $server) { continue; }
-
+		print("Domain: " . $mmail->nname . "\n");
 		$mmail->setUpdateSubaction('full_update');
 		$mmail->was();
 
 		$mlist = $mmail->getList('mailaccount');
 		foreach($mlist as $ml) {
 			$spam = $ml->getObject('spam');
+			print("Fixing: " . $ml->nname . "\n");
 			$spam->setUpdateSubaction('full_update');
 			$spam->was();
 			$ml->setUpdateSubaction('full_update');
@@ -36,3 +45,4 @@ foreach($list as $c) {
 	}
 }
 
+print("\nDone.\n");
