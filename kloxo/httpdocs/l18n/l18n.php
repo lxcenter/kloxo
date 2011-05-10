6561952fc219e82f420f11ce24a3f39b1fc1f031
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-05-10 22:13:49 +0000

    This code is working fine & Removed not needed dirs. See issue #397 for more info.

--- kloxo/httpdocs/l18n/l18n.php
+++ kloxo/httpdocs/l18n/l18n.php
@@ -1,84 +1,87 @@
 <?php
-# [FIXME] $locale = get_language(); and use $_SESSION for store language
-//$locale = '1';
-if ( isset( $_SERVER["HTTP_ACCEPT_LANGUAGE"] ) )
-	{
-		$locale = $_SERVER["HTTP_ACCEPT_LANGUAGE"];
-	}
-log_log('error',"localecheck. found agent $locale");
-/*
-if(isset($_REQUEST['locale'])){
-	switch($_REQUEST['locale']){
+
+if (isset($_REQUEST['locale'])) {
+	switch ($_REQUEST['locale']) {
 		case 'en':
-			$locale = 'en';
-		break;
+			$locale = 'en_US';
+			break;
 		case 'en_US':
 			$locale = 'en_US';
-		break;
+			break;
 		case 'es':
-			$locale = 'es';
-		break;
+			$locale = 'es_ES';
+			break;
 		case 'es_ES':
 			$locale = 'es_ES';
-		break;
+			break;
 		case 'nl':
-			$locale = 'nl';
-		break;
+			$locale = 'nl_NL';
+			break;
 		case 'nl_NL':
-			$locale = 'nl';
-		break;
+			$locale = 'nl_NL';
+			break;
 		default:
-			$locale = 'en';
-		break;
+			$locale = '';
+			break;
 	}
 
-	if(!empty($locale)){
+	if (!empty($locale)) {
 		$_SESSION['locale'] = $locale;
-	}
-	elseif(empty($_SESSION['locale'])){
+	} elseif (empty($_SESSION['locale'])) {
 		$_SESSION['locale'] = 'en_US';
 	}
 }
 
- elseif(empty($_SESSION['locale'])){
-	$_SESSION['locale'] = 'en_US';
+elseif (empty($_SESSION['locale'])) {
+	if (isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
+		$localex = explode(";", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+		$localex = explode(",", $localex['0']);
+		$locale = $localex['0'];
+		switch ($locale) {
+			case 'en':
+				$_SESSION['locale'] = 'en_US';
+				break;
+			case 'en-us':
+				$_SESSION['locale'] = 'en_US';
+				break;
+			case 'es':
+				$_SESSION['locale'] = 'es_ES';
+				break;
+			case 'es-es':
+				$_SESSION['locale'] = 'es_ES';
+				break;
+			case 'nl':
+				$_SESSION['locale'] = 'nl_NL';
+				break;
+			case 'nl-nl':
+				$_SESSION['locale'] = 'nl_NL';
+				break;
+			default:
+				$_SESSION['locale'] = 'en_US';
+				break;
+		}
+
+	} else {
+		$_SESSION['locale'] = 'en_US';
+	}
 }
- */
-$_SESSION['locale'] = $locale;
+
 $locale = $_SESSION['locale'];
-log_log('error',"Locale found. Defaults now to $locale");
+log_log('error', "Locale found. Language is set to $locale");
 
-if(empty($locale)){
-	$locale = 'en_US';
-	log_log('error',"no locale found. Defaults now to $locale");
+if (empty($locale)) {
+	$locale = "en_US";
+	log_log('error', "No locale found. Defaults now to $locale");
 
 }
 
-/*
- putenv("LC_ALL=$locale");
-
-$current_locale = setlocale(LC_ALL, $locale);
-if(empty($current_locale))
-{
-	dprint('Setting default locale to en_US');
-	$current_locale = 'en_US';
-	log_log('error',"XX no locale found. Defaults now to $current_locale");
-}
-dprint('Current locale: '.$current_locale.'<br />');
- */
-
-$locale_dir = '/usr/local/lxlabs/kloxo/httpdocs/l18n';
-setlocale(LC_MESSAGES, $locale);
-putenv("LANGUAGE=$locale");
+$locale_dir = $_SERVER["DOCUMENT_ROOT"] . "l18n";
 putenv("LANG=$locale");
-$gettext_domain = bindtextdomain('kloxo', $locale_dir);
-textdomain('kloxo');
+setlocale('LC_ALL', $locale);
+$gettext_domain = bindtextdomain("kloxo", $locale_dir);
+textdomain("kloxo");
 
-if(empty($gettext_domain)){
+if (empty($gettext_domain)) {
 	$error_msg = 'Could not load the text domain for translate strings.';
 	log_log('error', $error_msg);
 }
-else{
-	log_log('error','Gettext domain: '.$gettext_domain);
-}
-log_log('error','Text domain: '.textdomain('kloxo'));
