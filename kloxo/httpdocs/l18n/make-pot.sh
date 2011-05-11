ab1d92d9af0d4f65f398a91890d42e49a5f89eb2
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-05-11 01:57:38 +0000

    Issue #397, updated al po's from pot and generated mo's. Its a work in progress, things will be changed along the way.

--- kloxo/httpdocs/l18n/make-pot.sh
+++ kloxo/httpdocs/l18n/make-pot.sh
@@ -12,5 +12,14 @@ replace "# SOME DESCRIPTIVE TITLE." "# Kloxo master POT file" -- new.pot
 replace "# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER" "# Copyright (C) 2011 LxCenter" -- new.pot
 replace "# This file is distributed under the same license as the PACKAGE package." "# License AGPL-V3" -- new.pot
 replace "# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR." "# http://www.lxcenter.org/" -- new.pot
-ls -lh *.pot
-echo "Now replace kloxo.pot with new.pot to make it active."
+
+cp new.pot kloxo.pot
+
+msgmerge -U ./en_US/LC_MESSAGES/kloxo.po kloxo.pot
+msgmerge -U ./es_ES/LC_MESSAGES/kloxo.po kloxo.pot
+msgmerge -U ./nl_NL/LC_MESSAGES/kloxo.po kloxo.pot
+
+msgfmt ./en_US/LC_MESSAGES/kloxo.po -o ./en_US/LC_MESSAGES/kloxo.mo
+msgfmt ./es_ES/LC_MESSAGES/kloxo.po -o ./es_ES/LC_MESSAGES/kloxo.mo
+msgfmt ./nl_NL/LC_MESSAGES/kloxo.po -o ./nl_NL/LC_MESSAGES/kloxo.mo
+
