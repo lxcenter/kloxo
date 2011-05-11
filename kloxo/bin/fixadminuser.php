3ef9c18578318059c4b099ece8334bc38c911a38
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-05-11 23:45:05 +0000

    Merged r465 from trunk (#515).

--- kloxo/bin/fixadminuser.php
+++ kloxo/bin/fixadminuser.php
@@ -8,6 +8,10 @@ $list = posix_getpwnam('admin');
 if (!$list) {
 	os_create_system_user('admin', $login->password, 'admin', '/sbin/nologin', '/home/admin');
 	lxfile_unix_chown_rec("/home/admin", "admin");
+
+	//fixes issue #515
+	lxfile_generic_chmod("/home/admin", "0770");
+
 	lxshell_return("__path_php_path", "../bin/misc/fixwebdnsfullupdate.php");
 	lxshell_return("__path_php_path", "../bin/misc/fixftpuserclient.phps");
 }
