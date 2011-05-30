abdc506b776a75cda45dfe7bfe281cd7574f5697
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-05-30 15:11:12 +0000

    Added tar.gz archive for better compression. To be implemented in installer and upcp processes.

--- kloxo/make-distribution.sh
+++ kloxo/make-distribution.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 #
 # This file creates kloxo-[version].zip for distribution from SVN.
+# This file creates kloxo-[version].tar.gz for distribution from SVN.
 # 
 #
 # - read version
 # - compile c files
 # - create zip package
+# - create tar.gz package
+#
+# Changelog
+# may.30.2011 - Added tar.gz creation for better compression. .zip can be removed if kloxo installer/source is modified aswell.
+#
 ######
 echo "################################"
 echo "### Start packaging"
+
 echo "### read version..."
 # Read version
 # Please note, this must be a running machine with SVN version!
+
 if ! [ -f /script/version ] ; then
         echo "## Packaging failed. No /script/version found."
 	echo "## Are you sure you are running a development version?"
@@ -21,25 +29,43 @@ if ! [ -f /script/version ] ; then
 fi
 version=`/script/version`
 rm -f kloxo-$version.zip
-#
+
 echo "### Compile c files..."
-/bin/sh ./development-create-binaries.sh
-#
+
+	/bin/sh ./development-create-binaries.sh
+
 echo "### Create zip package..."
-# Package part
-zip -r9y kloxo-$version.zip ./bin ./cexe ./file ./httpdocs ./pscript ./sbin ./RELEASEINFO ./src -x \
-"*httpdocs/commands.php" \
-"*httpdocs/newpass" \
-"*httpdocs/.php.err" \
-"*/CVS/*" \
-"*/.svn/*" \
-"*httpdocs/thirdparty/*" \
-"*httpdocs/htmllib/extjs/*" \
-"*httpdocs/htmllib/fckeditor/*" \
-"*httpdocs/htmllib/yui-dragdrop/*"
-#
+
+	zip -r9y kloxo-$version.zip ./bin ./cexe ./file ./httpdocs ./pscript ./sbin ./RELEASEINFO ./src -x \
+	"*httpdocs/commands.php" \
+	"*httpdocs/newpass" \
+	"*httpdocs/.php.err" \
+	"*/CVS/*" \
+	"*/.svn/*" \
+	"*httpdocs/thirdparty/*" \
+	"*httpdocs/htmllib/extjs/*" \
+	"*httpdocs/htmllib/fckeditor/*" \
+	"*httpdocs/htmllib/yui-dragdrop/*"
+
+echo "### Created ZIP package"
+
+echo "### Create TAR.GZ package"
+
+        tar cvfz kloxo-$version.tar.gz \
+        ./bin ./cexe ./file ./httpdocs ./pscript ./sbin ./RELEASEINFO ./src \
+	--exclude="commands.php" \
+	--exclude="newpass" \
+	--exclude=".php.err" \
+	--exclude="thirdparty" \
+	--exclude="extjs" \
+	--exclude="fckeditor" \
+	--exclude="yui-dragdrop" \
+        --exclude="CVS" \
+        --exclude=".svn"
+
+echo "### Created TAR.GZ package"
 echo "### Finished"
 echo "################################"
-ls -lh kloxo-*.zip
+ls -lh kloxo-*
 #
 
