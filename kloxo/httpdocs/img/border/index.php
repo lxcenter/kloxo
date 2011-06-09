43d143fec05abc55595ccccd9987c5176e65190a
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-06-09 14:00:53 +0000

    Cleaned the IMG directory for deletion of unused images (In prepare for #14)

--- kloxo/httpdocs/img/border/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo"Hello World";
-?>
-<div style='background:url(/dot.gif) 0 0 repeat-x; width: 20em'><div style='background: url(/dot.gif) 0 100% repeat-x'><div style='background: url(/dot.gif) 0 0 repeat-y'><div style='background: url(/dot.gif) 100% 0 repeat-y'><div style='background: url(/bl.gif) 0 100% no-repeat'><div style='background: url(/br.gif) 100% 100% no-repeat'><div style='background: url(/tl.gif) 0 0 no-repeat'><div style='background: url(/tr.gif) 100% 0 no-repeat; padding:10px'>Lorem ipsum dolor sit amet consectetur adipisicing elit</div></div></div></div></div></div></div></div> 
-
