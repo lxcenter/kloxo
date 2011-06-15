d009e7d13d7c905c3172eba2ce8626a96c021551
jisse03 <jisse03@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-06-15 06:28:21 +0000

    Updated translation.

--- kloxo/httpdocs/lang/fr/langfunctionlib.php
+++ kloxo/httpdocs/lang/fr/langfunctionlib.php
@@ -2,7 +2,6 @@
 // Translation done by pob944
 function get_plural($word)
 {
-	return $word ;
 	if ($word[strlen($word) - 1] === 's') {
 		$ret = "{$word}es";
 	} else if ($word[strlen($word) - 1] === 'y') {
