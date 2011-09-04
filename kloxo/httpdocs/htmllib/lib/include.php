ec2bac5959929654a4a4d9011dde1743b795ff59
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-09-04 16:36:22 +0000

    Enable the new localize for the rest of Kloxo 6.2 code (#397) and revert Mustafa code in lxlib to 6.2 code.

--- kloxo/httpdocs/htmllib/lib/include.php
+++ kloxo/httpdocs/htmllib/lib/include.php
@@ -24,6 +24,10 @@ include_once "lib/common.inc";
 // This is the program specific common lib. There is no need dump everything htmllib/lib/lib.php which has become too large.
 include_once "lib/programlib.php";
 
+// New Localize system (Kloxo 6.2.x) Issue #397
+include_once "i18n/i18n.php";
+init_language();
+
 if (lxfile_exists("../etc/classdefine")) {
 	$list = lscandir_without_dot("../etc/classdefine");
 	foreach($list as $l) {
