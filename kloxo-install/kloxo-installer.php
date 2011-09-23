28fea28a7a698ed0168796a2e076313498f9d987
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-09-23 00:48:01 +0000

    Issue #645 - New installer script (also packer and patcher) for Kloxo 6.1.7 (directly to branches/6.1.x after merge url svn trunk to branches/6.1.x local copy)

---
+++ kloxo-install/kloxo-installer.php
@@ -0,0 +1,549 @@
+<?php
+//    Kloxo, Hosting Control Panel
+//
+//    Copyright (C) 2000-2009	LxLabs
+//    Copyright (C) 2009-2010	LxCenter
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// ==== kloxo_installer portion ===
+
+lxins_main();
+
+function lxins_main()
+{
+	global $argv, $downloadserver;
+	$opt = parse_opt($argv);
+	$dir_name=dirname(__FILE__);
+	$installtype = $opt['install-type'];
+	$dbroot = isset($opt['db-rootuser'])? $opt['db-rootuser']: "root";
+	$dbpass = isset($opt['db-rootpassword'])? $opt['db-rootpassword']: "";
+	$osversion = find_os_version();
+	$arch = `arch`;
+	$arch = trim($arch);
+	
+	//--- create temporal flags for install
+	system("mkdir -p /var/cache/kloxo/");
+	system("echo 1 > /var/cache/kloxo/kloxo-install-firsttime.flg");
+
+	if (!char_search_beg($osversion, "centos") && !char_search_beg($osversion, "rhel")) {
+		print("Kloxo is only supported on CentOS 5 and RHEL 5\n");
+		exit;
+	}
+
+	if(file_exists("/usr/local/lxlabs/kloxo")) {
+		// Ask Reinstall
+		if (get_yes_no("Kloxo seems already installed do you wish to continue?") == 'n') {
+			print("Installation Aborted.\n");
+			exit;
+		}
+	} else {
+		// Ask License
+		if (get_yes_no("Kloxo is using AGPL-V3.0 License, do you agree with the terms?") == 'n') {
+			print("You did not agree to the AGPL-V3.0 license terms.\n");
+			print("Installation aborted.\n\n");
+			exit;
+		} else {
+			print("Installing Kloxo = YES\n\n");
+		}
+	}
+	// Ask for InstallApp
+	print("InstallApp: PHP Applications like PHPBB, WordPress, Joomla etc\n");
+	print("When you choose Yes, be aware of downloading about 350Mb of data!\n");
+	if(get_yes_no("Do you want to install the InstallAPP sotfware?") == 'n') {
+		print("Installing InstallApp = NO\n");
+		print("You can install it later with /script/installapp-update\n\n");
+		$installappinst = false;
+		//--- temporal flag for no install InstallApp
+		system("echo 1 > /var/cache/kloxo/kloxo-install-disableinstallapp.flg");
+	} else {
+		print("Installing InstallApp = YES\n\n");
+		$installappinst = true;
+	}
+
+	print("Adding System users and groups (nouser, nogroup and lxlabs, lxlabs)\n");
+	system("groupadd nogroup");
+	system("useradd nouser -g nogroup -s '/sbin/nologin'");
+	system("groupadd lxlabs");
+	system("useradd lxlabs -g lxlabs -s '/sbin/nologin'");
+
+	print("Installing LxCenter yum repository for updates\n");
+	install_yum_repo($osversion);
+
+	$packages = array("sendmail", "sendmail-cf", "sendmail-doc", "sendmail-devel", "exim", "vsftpd", "postfix", "vpopmail", "qmail", "lxphp", "lxzend", "pure-ftpd", "imap");
+
+	$list = implode(" ", $packages);
+	print("Removing packages $list...\n");
+	foreach ($packages as $package) {
+		exec("rpm -e --nodeps $package > /dev/null 2>&1");
+	}
+
+	$packages = array("php-mbstring", "php-mysql", "which", "gcc-c++", "php-imap", "php-pear", "php-devel", "lxlighttpd", "httpd", "mod_ssl", "zip", "unzip", "lxphp", "lxzend", "mysql", "mysql-server", "curl", "autoconf", "automake", "libtool", "bogofilter", "gcc", "cpp", "openssl", "pure-ftpd", "yum-protectbase");
+
+	$list = implode(" ", $packages);
+
+	while (true) {
+		print("Installing packages $list...\n");
+		system("PATH=\$PATH:/usr/sbin yum -y install $list", $return_value);
+
+		if (file_exists("/usr/local/lxlabs/ext/php/php")) {
+			break;
+		}
+		else {
+			print("Yum Gave Error... Trying Again...\n");
+			if (get_yes_no("Try again?") == 'n') {
+				print("- EXIT: fix the problem and install again\n");			
+				exit;
+			}
+		}
+	}
+
+	print("Prepare installation directory\n");
+	
+	system("mkdir -p /usr/local/lxlabs/kloxo");
+
+	if (file_exists("../kloxo-current.zip")) {
+		//--- that mean install with local copy
+		@ unlink("/usr/local/lxlabs/kloxo/kloxo-current.zip");
+		print("Local copying Kloxo release\n");
+		passthru("mkdir -p /var/cache/kloxo");
+		passthru("cp -rf ../kloxo-current.zip /usr/local/lxlabs/kloxo");
+
+		// the first step - remove 
+		passthru("rm -f /var/cache/kloxo/kloxo-thirdparty*.zip");
+		passthru("rm -f /var/cache/kloxo/lxawstats*.tar.gz");
+		passthru("rm -f /var/cache/kloxo/lxwebmail*.tar.gz");
+		passthru("rm -f /var/cache/kloxo/kloxophpsixfour*.tar.gz");
+		passthru("rm -f /var/cache/kloxo/kloxophp*.tar.gz");
+		passthru("rm -f /var/cache/kloxo/*-version");
+		// the second step - copy from packer making if exist
+		passthru("cp -rf ../kloxo-thirdparty*.zip /var/cache/kloxo");
+		passthru("cp -rf ../lxawstats*.tar.gz /var/cache/kloxo");
+		passthru("cp -rf ../lxwebmail*.tar.gz /var/cache/kloxo");
+		passthru("cp -rf ../kloxo-thirdparty-version /var/cache/kloxo");
+		passthru("cp -rf ../lxawstats-version /var/cache/kloxo");
+		passthru("cp -rf ../lxwebmail-version /var/cache/kloxo"); 
+		if (file_exists("/usr/lib64")) {
+			if (!is_link("/usr/lib/kloxophp")) {
+				passthru("rm -rf /usr/lib/kloxophp");
+			}
+			passthru("cp -rf ../kloxophpsixfour*.tar.gz /var/cache/kloxo");
+			passthru("cp -rf ../kloxophpsixfour-version /var/cache/kloxo");
+			passthru("mkdir -p /usr/lib64/kloxophp");
+			passthru("ln -s /usr/lib64/kloxophp /usr/lib/kloxophp");
+			passthru("mkdir -p /usr/lib64/php");
+			passthru("ln -s /usr/lib64/php /usr/lib/php");
+			passthru("mkdir -p /usr/lib64/httpd");
+			passthru("ln -s /usr/lib64/httpd /usr/lib/httpd");
+			passthru("mkdir -p /usr/lib64/lighttpd");
+			passthru("ln -s /usr/lib64/lighttpd /usr/lib/lighttpd");
+		}
+		else {
+			//--- use this trick because lazy to make code for version check
+			passthru("rename ../kloxophpsixfour ../_kloxophpsixfour ../kloxophpsixfour*");
+			passthru("cp -rf ../kloxophp*.tar.gz /var/cache/kloxo");
+			passthru("rename ../_kloxophpsixfour ../kloxophpsixfour ../_kloxophpsixfour*");
+			passthru("cp -rf ../kloxophp-version /var/cache/kloxo"); 
+		}
+		chdir("/usr/local/lxlabs/kloxo");
+		passthru("mkdir -p /usr/local/lxlabs/kloxo/log");
+	}
+	else {
+		chdir("/usr/local/lxlabs/kloxo");
+		system("mkdir -p /usr/local/lxlabs/kloxo/log");
+		unlink("kloxo-current.zip");
+		print("Downloading latest Kloxo release\n");
+		system("wget ".$downloadserver."download/kloxo/production/kloxo/kloxo-current.zip");
+	}
+
+	print("\n\nInstalling Kloxo.....\n\n");
+	system("unzip -oq kloxo-current.zip", $return);
+
+	if ($return) {
+		print("Unzipping the core Failed.. Most likely it is corrupted. Report it at http://forum.lxcenter.org/\n");
+		exit;
+	}
+
+	unlink("kloxo-current.zip");
+	system("chown -R lxlabs:lxlabs /usr/local/lxlabs/");
+	chdir("/usr/local/lxlabs/kloxo/httpdocs/");
+	system("service mysqld start");
+
+	if ($installtype !== 'slave') {
+		check_default_mysql($dbroot, $dbpass);
+	}
+
+	$mypass = password_gen();
+
+	print("Prepare defaults and configurations...\n");
+//	system("/usr/local/lxlabs/ext/php/php $dir_name/kloxo_all.php");
+	install_main();
+
+	file_put_contents("/etc/sysconfig/spamassassin", "SPAMDOPTIONS=\" -v -d -p 783 -u lxpopuser\"");
+	print("\nCreating Vpopmail database...\n");
+	system("sh $dir_name/kloxo-linux/vpop.sh $dbroot \"$dbpass\" lxpopuser $mypass");
+	system("chmod -R 755 /var/log/httpd/");
+	system("chmod -R 755 /var/log/httpd/fpcgisock >/dev/null 2>&1");
+	system("mkdir -p /var/log/kloxo/");
+	system("mkdir -p /var/log/news");
+	system("ln -sf /var/qmail/bin/sendmail /usr/sbin/sendmail");
+	system("ln -sf /var/qmail/bin/sendmail /usr/lib/sendmail");
+	system("echo `hostname` > /var/qmail/control/me");
+	system("service qmail restart >/dev/null 2>&1 &");
+	system("service courier-imap restart >/dev/null 2>&1 &");
+
+	print("Prepare /home/kloxo/httpd...\n");
+	system("mkdir -p /home/kloxo/httpd");
+	chdir("/home/kloxo/httpd");
+	@ unlink("skeleton-disable.zip");
+	system("chown -R lxlabs:lxlabs /home/kloxo/httpd");
+	system("/etc/init.d/kloxo restart >/dev/null 2>&1 &");
+	chdir("/usr/local/lxlabs/kloxo/httpdocs/");
+	system("/usr/local/lxlabs/ext/php/php /usr/local/lxlabs/kloxo/bin/install/create.php --install-type=$installtype --db-rootuser=$dbroot --db-rootpassword=$dbpass");
+
+	if ($installappinst) {
+		print("Install InstallApp...\n");
+		system("/script/installapp-update"); // First run (gets installappdata)
+		system("/script/installapp-update"); // Second run (gets applications)
+	}
+
+	// --- remove all temporal flags because the end of install
+	print("Remove Kloxo install flags...\n");
+	system("rm -rf /var/cache/kloxo/*-version");
+	system("rm -rf /var/cache/kloxo/kloxo-install-*.flg");
+
+	//--- for prevent mysql socket problem (especially on 64bit system)
+	if (!file_exists("/var/lib/mysql/mysql.sock")) {
+		print("Create mysql.sock...\n");
+		system("/etc/init.d/mysqld stop");
+		system("mksock /var/lib/mysql/mysql.sock");	
+		system("/etc/init.d/mysqld start");
+	}
+
+	// --- fix for old version (before 6.1.7) -- thirdparty
+	if (!file_exists("/usr/local/lxlabs/kloxo/httpdocs/thirdparty")) {
+		// cp from ../ not work, so use /var/cache/kloxo
+		system("cp -rf /var/cache/kloxo/kloxo-thirdparty*.zip /usr/local/lxlabs/kloxo");
+		system("cd /usr/local/lxlabs/kloxo; unzip -oq kloxo-thirdparty*.zip");
+		system("chown -R lxlabs:lxlabs /usr/local/lxlabs/kloxo/httpdocs/thirdparty");
+		system("chown -R lxlabs:lxlabs /usr/local/lxlabs/kloxo/httpdocs/htmllib");
+		system("rm -f /usr/local/lxlabs/kloxo/kloxo-thirdparty*.zip");
+	}
+
+	// --- to make sure ownership of httpdocs dir
+	system("chown -R lxlabs:lxlabs /usr/local/lxlabs/kloxo/httpdocs");
+
+	print("\nCongratulations. Kloxo has been installed succesfully on your server as $installtype\n\n");
+	if ($installtype === 'master') {
+		print("You can connect to the server at:\n");
+		print("	https://<ip-address>:7777 - secure ssl connection, or\n");
+		print("	http://<ip-address>:7778 - normal one.\n\n");
+		print("The login and password are 'admin' 'admin'. After Logging in, you will have to\n");
+		print("change your password to something more secure\n\n");
+		print("We hope you will find managing your hosting with Kloxo\n");
+		print("refreshingly pleasurable, and also we wish you all the success\n");
+		print("on your hosting venture\n\n");
+		print("Thanks for choosing Kloxo to manage your hosting, and allowing us to be of\n");
+		print("service\n");
+	} else {
+		print("You should open the port 7779 on this server, since this is used for\n");
+		print("the communication between master and slave\n\n");
+		print("To access this slave, to go admin->servers->add server,\n");
+		print("give the ip/machine name of this server. The password is 'admin'.\n\n");
+		print("The slave will appear in the list of slaves, and you can access it\n");
+		print("just like you access localhost\n\n");
+	}
+	print("\n");
+	print("---------------------------------------------\n");
+	print("* Note: Better reboot after Kloxo install\n\n");
+}
+
+// ==== kloxo_all portion ===
+
+function install_general_mine($value) {
+	$value = implode(" ", $value);
+	print("Installing $value ....\n");
+	system("PATH=\$PATH:/usr/sbin yum -y install $value");
+}
+
+function installcomp_mail() {
+	system('pear channel-update "pear.php.net"'); // to remove old channel warning
+	system("pear upgrade --force pear"); // force is needed
+	system("pear upgrade --force Archive_Tar"); // force is needed
+	system("pear upgrade --force structures_graph"); // force is needed
+	system("pear install log");
+}
+
+function install_main() {
+
+	$installcomp['mail'] = array("vpopmail", "courier-imap-toaster", "courier-authlib-toaster", "qmail", "safecat", "httpd", "spamassassin", "ezmlm-toaster", "autorespond-toaster");
+	$installcomp['web'] = array("httpd", "pure-ftpd");
+	$installcomp['dns'] = array("bind", "bind-chroot");
+	$installcomp['database'] = array("mysql");
+
+	global $argv;
+	$comp = array("web", "mail", "dns", "database");
+
+	$serverlist = $comp;
+
+	foreach ($comp as $c) {
+	    flush();
+	    if (array_search($c, $serverlist) !== false) {
+	   	 print("Installing $c Components....");
+	   	 $req = $installcomp[$c];
+	   	 $func = "installcomp_$c";
+	   	 if (function_exists($func)) {
+	   	     $func();
+	   	 }
+	   	 install_general_mine($req);
+	   	 print("\n");
+	    }
+	}
+
+	$options_file = "/var/named/chroot/etc/global.options.named.conf";
+
+	$example_options  = "acl \"lxcenter\" {\n";
+	$example_options .= "\tlocalhost;\n";
+	$example_options .= "};\n\n";
+	$example_options .= "options {\n";
+	$example_options .= "\tmax-transfer-time-in 60;\n";
+	$example_options .= "\ttransfer-format many-answers;\n";
+	$example_options .= "\ttransfers-in 60;\n";
+	$example_options .= "\tauth-nxdomain yes;\n";
+	$example_options .= "\tallow-transfer { \"lxcenter\"; };\n";
+	$example_options .= "\tallow-recursion { \"lxcenter\"; };\n";
+	$example_options .= "\trecursion no;\n";
+	$example_options .= "\tversion \"LxCenter-1.0\";\n";
+	$example_options .= "};\n\n";
+	$example_options .= "# Remove # to see all DNS queries\n";
+	$example_options .= "# logging {\n";
+	$example_options .= "#\t channel query_logging {\n";
+	$example_options .= "#\t\t file \"/var/log/named_query.log\";\n";
+	$example_options .= "#\t\t versions 3 size 100M;\n";
+	$example_options .= "#\t\t print-time yes;\n";
+	$example_options .= "#\t };\n\n";
+	$example_options .= "#\t category queries {\n";
+	$example_options .= "#\t\t query_logging;\n";
+	$example_options .= "#\t };\n";
+	$example_options .= "# };\n";
+
+	if (!file_exists($options_file)) {
+		touch($options_file);
+		chown($options_file, "named");
+	}
+
+	$cont = file_get_contents($options_file);
+	$pattern = "options";
+
+	if (!preg_match("+$pattern+i", $cont)) {
+		file_put_contents($options_file, "$example_options\n");
+	}
+
+	$pattern = 'include "/etc/kloxo.named.conf";';
+	$file = "/var/named/chroot/etc/named.conf";
+	$comment = "//Kloxo";
+	addLineIfNotExist($file, $pattern, $comment);
+	touch("/var/named/chroot/etc/kloxo.named.conf");
+	chown("/var/named/chroot/etc/kloxo.named.conf", "named");
+
+}
+
+// ==== kloxo_common portion ===
+
+class remote { }
+
+$downloadserver = "http://download.lxcenter.org/";
+
+function slave_get_db_pass() {
+	$file = "/usr/local/lxlabs/kloxo/etc/slavedb/dbadmin";
+	if (!file_exists($file)) {
+		return null;
+	}
+	$var = file_get_contents($file);
+	$rmt = unserialize($var);
+	return $rmt->data['mysql']['dbpassword'];
+}
+
+function check_default_mysql($dbroot, $dbpass) {
+	system("service mysqld restart");
+
+	if ($dbpass) {
+		exec("echo \"show tables\" | mysql -u $dbroot -p\"$dbpass\" mysql", $out, $return);
+	} else {
+		exec("echo \"show tables\" | mysql -u $dbroot mysql", $out, $return);
+	}
+
+	if ($return) {
+		resetDBPassword($dbroot, $dbpass);
+	}
+}
+
+function parse_opt($argv) {
+	unset($argv[0]);
+	if (!$argv) {
+		return null;
+	}
+	foreach ($argv as $v) {
+		if (strstr($v, "=") === false || strstr($v, "--") === false) {
+			continue;
+		}
+		$opt = explode("=", $v);
+		$opt[0] = substr($opt[0], 2);
+		$ret[$opt[0]] = $opt[1];
+	}
+	return $ret;
+}
+
+function password_gen() {
+	$data = mt_rand(2, 30);
+	$pass = "lx" . $data; // lx is a indentifier
+	return $pass;
+}
+
+function char_search_beg($haystack, $needle) {
+	if (strpos($haystack, $needle) === 0) {
+		return true;
+	}
+	return false;
+}
+
+function install_yum_repo($osversion) {
+
+	global $dirpath;
+
+	if (!file_exists("/etc/yum.repos.d")) {
+		print("No yum.repos.d dir detected!\n");
+		return;
+	}
+	if (file_exists("/etc/yum.repos.d/lxcenter.repo")) {
+		print("LxCenter yum repository file already present.\n");
+		return;
+	}
+
+	$cont = file_get_contents("lxcenter.repo.template");
+	$cont = str_replace("%distro%", $osversion, $cont);
+	file_put_contents("/etc/yum.repos.d/lxcenter.repo", $cont);
+}
+
+function find_os_version() {
+	// list os support
+	$ossup = array('redhat' => 'rhel', 'fedora' => 'fedora', 'centos' => 'centos');
+	
+	foreach(array_keys($ossup) as $k) {
+		$osrel = file_get_contents("/etc/{$k}-release");
+		if ($osrel) {
+				$osrel = strtolower(trim($osrel));
+				break;
+		}
+	}
+	
+	// specific for 'red hat'
+	$osrel = str_replace('red hat', 'redhat', $osrel);
+
+	$osver = explode(" ", $osrel);
+
+	$verpos = sizeof($osver) - 2;
+
+	if (array_key_exists($osver[0], $ossup)) {
+		// specific for 'red hat'
+		if ($osrel === 'redhat') {
+			$oss = $osver[$verpos];
+		}
+		else {
+			$mapos = explode(".", $osver[$verpos]);
+			$oss = $mapos[0];
+		}
+		return $ossup[$osver[0]]."-".$oss;
+	}
+	else {
+		print("This Operating System is currently *NOT* supported.\n");
+		exit;
+	}
+
+}
+
+/**
+ * Get Yes/No answer from stdin
+ * @param string $question question text
+ * @param char $default default answer (optional)
+ * @return char 'y' for Yes or 'n' for No
+ */
+function get_yes_no($question, $default = 'n') {
+	if ($default != 'y') {
+		$default = 'n';
+		$question .= ' [y/N]: ';
+	} else {
+		$question .= ' [Y/n]: ';
+	}
+	for (;;) {
+		print $question;
+		flush();
+		$input = fgets(STDIN, 255);
+		$input = trim($input);
+		$input = strtolower($input);
+		if ($input == 'y' || $input == 'yes' || ($default == 'y' && $input == '')) {
+			return 'y';
+		}
+		else if ($input == 'n' || $input == 'no' || ($default == 'n' && $input == '')) {
+			return 'n';
+		}
+	}
+}
+
+// --- taken from reset-mysql-root-password.phps
+function resetDBPassword($user, $pass)
+{
+	print("Stopping MySQL\n");
+	shell_exec("service mysqld stop");
+	print("Start MySQL with skip grant tables\n");
+	shell_exec("su mysql -c \"/usr/libexec/mysqld --skip-grant-tables\" >/dev/null 2>&1 &");
+	print("Using MySQL to flush privileges and reset password\n");
+	sleep(10);
+	system("echo \"update user set password = Password('{$pass}') where User = '{$user}'\" | mysql -u [$user} mysql ", $return);
+
+	while($return) {
+		print("MySQL could not connect, will sleep and try again\n");
+		sleep(10);
+		system("echo \"update user set password = Password('{$pass}') where User = '{$user}'\" | mysql -u {$user} mysql", $return);
+	}
+
+	print("Password reset succesfully. Now killing MySQL softly\n");
+	shell_exec("killall mysqld");
+	print("Sleeping 10 seconds\n");
+	shell_exec("sleep 10");
+	print("Restarting the actual MySQL service\n");
+	system("service mysqld restart");
+	print("Password successfully reset to \"$pass\"\n");
+}
+
+function addLineIfNotExist($filename, $pattern, $comment) {
+
+	if (file_exists($filename)) {
+		$cont = file_get_contents($filename);
+	} else {
+		$cont = '';
+	}
+
+	if (!preg_match("+$pattern+i", $cont)) {
+		file_put_contents($filename, "\n$comment \n\n", FILE_APPEND);
+		file_put_contents($filename, $pattern, FILE_APPEND);
+		file_put_contents($filename, "\n\n\n", FILE_APPEND);
+	} else {
+		print("Pattern '$pattern' Already present in $filename\n");
+	}
+
+
+}
+
--- kloxo-install/kloxo-installer.sh
+++ kloxo-install/kloxo-installer.sh
@@ -20,20 +20,34 @@
 #
 # LxCenter - Kloxo Installer
 #
-# TODO: Add options to reset mysql root pass, check free disk space,
-#		uninstall. Maybe use "dialog" for ncurses-like functions.
+# Version: 1.0 (2011-08-02 - by mustafa.ramadhan@lxcenter.org)
 #
 
-# change to kloxo-installer.sh since 6.1.7
-
-echo "* Use kloxo-installer.sh and download from:"
-echo "    wget http://download.lxcenter.org/download/kloxo/production/kloxo-installer.sh"
-echo
-echo "* download kloxo-packer.sh for pack local copy and kloxo-patcher.sh for patch"
-exit;
+if [ "$#" == 0 ] ; then
+	echo
+	echo " -------------------------------------------------------------------"
+	echo "  format: sh $0 --type=[]"
+	echo " -------------------------------------------------------------------"
+	echo "  --type - master or slave"
+	echo
+	echo " * Run kloxo-packer.sh to make kloxo packs"
+	echo " * This installer only test on 6.1.7+ version."
+	echo
+	exit;
+fi
 
 APP_NAME=Kloxo
-APP_TYPE=Master
+
+request1=$1
+APP_TYPE=${request1#--type\=}
+
+if [ ! $APP_TYPE == 'master' ] && [ ! $APP_TYPE == 'slave' ] ; then
+	echo "Wrong --type= entry..."
+	exit;
+fi
+
+request2=$2
+DB_ROOTPWD=${request2#--db-rootpassword\=}
 
 SELINUX_CHECK=/usr/sbin/selinuxenabled
 SELINUX_CFG=/etc/selinux/config
@@ -109,7 +123,8 @@ fi
 # Check if selinuxenabled exists
 if [ ! -f $SELINUX_CHECK ] ; then
     echo -en "SELinux disabled             " $C_MISS
-    echo -e "\a\nThe installer could not determine SELinux status.\nIf you are sure it is DISABLED, you may proceed."
+    echo -e "\a\nThe installer could not determine SELinux status.\n" \
+		"If you are sure it is DISABLED, you may proceed."
     get_yes_no "Continue?" 0
     if [ "$?" -eq "0" ] ; then 
         echo -e "Aborting ...\n"
@@ -142,26 +157,19 @@ else
     fi
 fi
 
-# Check if OS is 32bit and if not allow user to choose to continue or not (for devels).
-# Remove this when RHEL/CENTOS x86_64 is officially supported or add the arch to prevent people from installing in ARM.
-if [ "$ARCH_CHECK" != "i686" ] ; then
-    echo -en "\aArchitecture supported ($ARCH_CHECK)" $C_NO "\n"
-    echo -e "Your OS architecture ($ARCH_CHECK) is NOT officially supported yet and $APP_NAME may not work correctly."
-    get_yes_no "Continue anyway?" 0
-    if [ "$?" -eq "0" ] ; then 
-        echo -e "Aborting ...\n"
-        exit $E_ARCH
-    fi
-else
-    echo -en "Architecture supported ($ARCH_CHECK)" $C_OK
-fi
-
 # Check for mysql databases and arguments.
 if  [ -d /var/lib/mysql ] && [ -z "$1" ] ; then
     echo -en "Database and arguments check " $C_NO
+    if [ `echo $0 || grep 'kloxo-install.sh' ` ] ; then
+        txtsh=`sh $0 --type=master/slave --db-rootpassword=PASSWORD`
+    else
+        txtsh=`sh $0 --db-rootpassword=PASSWORD`
+    fi
+
     echo -e "\a\nIt seems you already have databases in this system but did not provide the MySQL root pass. " \
 		"If you are reinstalling, remove mysql-server and databases stored at /var/lib/mysql. " \
-		"Otherwise, you must provide the password.\n\nUsage: sh $0 --db-rootpassword=PASSWORD\n\nAborting ...\n"
+		"Otherwise, you must provide the password.\n\nUsage: $txtsh\n\nAborting ...\n"
+
     exit $E_HASDB
 else
     echo -en "Database and arguments check " $C_OK
@@ -184,15 +192,22 @@ echo -e "    When it's finished, you will be presented with a welcome message an
 read -n 1 -p "Press any key to continue ..."
 
 yum -y install php php-mysql wget zip unzip
-
 export PATH=/usr/sbin:/sbin:$PATH
 
-if [! -f ./kloxo-install.zip ] ; then
+if [ ! -f ./kloxo-install.zip ] ; then
     wget http://download.lxcenter.org/download/kloxo-install.zip
 fi
 
-unzip -oq kloxo-install.zip
+if [ -d kloxo-install ] ; then
+    cd kloxo-install
+else
+    unzip -oq kloxo-install.zip
+    cd kloxo-install
+fi
 
-cd kloxo-install/kloxo-linux
+if [ -f /usr/local/lxlabs/ext/php/php ] ; then
+	/usr/local/lxlabs/ext/php/php kloxo-installer.php --install-type=$APP_TYPE $* | tee kloxo_install.log
+else
+	php kloxo-installer.php --install-type=$APP_TYPE $* | tee kloxo_install.log
+fi
 
-php lxins.php --install-type=master $* | tee kloxo_install.log
