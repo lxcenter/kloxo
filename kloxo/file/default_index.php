7e2c311dbe4d4cbd1b7e6b3191a5d4d3b3c05162
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-09-23 00:58:52 +0000

    Issue #646 - New webpage for default/disable/cp/webmail page on Kloxo 6.1.7 (directly to branches/6.1.x after merge url svn trunk to branches/6.1.x local copy)

---
+++ kloxo/file/default_index.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+<title>Kloxo Control Panel</title>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+</head>
+
+<style>
+body {
+	font-family: Tahoma, Verdana, Arial, Helvertica, sans-serif;
+	font-size: 1em;
+	background: #ddeeff;
+	margin: 0;
+}
+a {
+	text-decoration: none;
+}
+img {
+	border: 0;
+}
+</style>
+<body>
+
+<table cellpadding="0" cellspacing="0" width="100%" border="0" bgcolor="#66aaddd" height="100">
+	<tr>
+		<td valign="top"><img src="images/logo.png" height="75" vspace="5" hspace="5" alt="hosting-logo"></td>
+		<td width="130">
+		<table border="0" width="100%" cellspacing="0" cellpadding="0">
+					<tr>
+						<td><a href="http://lxcenter.org/" title="Go to LxCenter website">
+						<img src="images/lxcenter.png" alt="lxcenter-logo" width="120" height="35" hspace="5" vspace="5"></a></td>
+					</tr>
+					<tr>
+						<td><a href="http://lxcenter.org/software/kloxo/" title="Go to Kloxo website">
+						<img src="images/kloxo.png" alt="kloxo-logo" width="120" height="27" hspace="5" vspace="5"></a></td>
+					</tr>
+					</table></td>
+	</tr><tr><td colspan="2" bgcolor="#000000">&nbsp;</td></tr>
+</table>
+<table width="100%" height="300" cellpadding="0" cellspacing="0">
+	<tr>
+		<td width="50">&nbsp;</td>
+		<td valign="top"><?php include_once "./inc.php"; ?></td>
+		<td width="280" valign="center">
+		<img src="images/disableskeletonbg.gif"></td>
+	</tr>
+</table>
+
+</body>
+
+</html>
