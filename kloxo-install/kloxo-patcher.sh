d56f62de12380d81e16b040ac7953d550eab6290
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-09-23 01:47:21 +0000

    Issue #645 - modified kloxo-packer.sh (more interactive). the same with latest branches update

--- kloxo-install/kloxo-patcher.sh
+++ kloxo-install/kloxo-patcher.sh
@@ -23,6 +23,227 @@
 # Version: 1.0 (2011-08-02 - by mustafa.ramadhan@lxcenter.org)
 #
 
+### functions - must declare before execute ###
+
+# Reads yes|no answer from the input ; question text ; default answer, yes = 1 and no = 0
+function get_yes_no () {
+    local question=
+    local input=
+    case $2 in 
+        1 ) question="$1 [Y/n]: "
+            ;;
+        0 ) question="$1 [y/N]: "
+            ;;
+        * ) question="$1 [y/n]: "
+    esac
+
+    while :
+    do
+        read -p "$question" input
+        input=$( echo $input | tr -s '[:upper:]' '[:lower:]' )
+        if [ "$input" = "" ] ; then
+            if [ "$2" == "1" ] ; then
+                return 1
+            elif [ "$2" == "0" ] ; then
+                return 0
+            fi
+        else
+            case $input in
+                y|yes) return 1
+                    ;;
+                n|no) return 0
+                    ;;
+            esac
+        fi
+    done
+}
+
+function kloxo_core_portion () {
+	echo
+	echo "Step for Kloxo Core..."
+
+	if [ -f ./patch/kloxo-patch-version ] ; then
+		patchver=`cat ./patch/kloxo-patch-version`
+	else
+		patchver=""
+	fi
+
+	kloxover=`sh /script/version`
+	
+	kloxo_check_version $patchver $kloxover
+	
+	if [ "$?" -eq "1" ] ; then
+		echo "- Set ownership and permissions"
+		chown -R lxlabs:lxlabs ./patch/kloxo/
+		find ./patch/kloxo/ -type f -name \"*.php*\" -exec chmod 644 {} \;
+		find ./patch/kloxo/ -type d -exec chmod 755 {} \;
+
+		echo "- Copy patch file"
+		cp -rf ./patch/kloxo/* /usr/local/lxlabs/kloxo
+		cp -rf /usr/local/lxlabs/kloxo/pscript /script
+		cp -rf /usr/local/lxlabs/kloxo/httpdocs/htmllib/script /script
+
+		kloxo_64bit
+	else
+		kloxo_exit
+	fi
+}
+
+function kloxo_64bit () {
+	if [ -f /usr/lib64 ] ; then
+		echo "- Set symlink for 3bit from 64bit links"
+
+		if [ ! -h /usr/lib/kloxophp ] ; then
+			echo "- /usr/lib/kloxophp not as symlink, deleted"
+			rm -rf /usr/lib/kloxophp
+		fi
+	
+		echo "- Set symlink for 64bit version"
+		mkdir -p /usr/lib64/php
+		ln -s /usr/lib64/php /usr/lib/php
+		mkdir -p /usr/lib64/httpd
+		ln -s /usr/lib64/httpd /usr/lib/httpd
+		mkdir -p /usr/lib64/lighttpd
+		ln -s /usr/lib64/lighttpd /usr/lib/lighttpd
+		mkdir -p /usr/lib64/kloxophp
+		ln -s /usr/lib64/kloxophp /usr/lib/kloxophp
+	else
+		echo "- No extra setting for 32bit"
+	fi
+}
+
+function kloxo_thirdparty_portion () {
+	echo "Step for Kloxo Thirdparty..."
+
+	if [ -f ./patch/thirdparty-patch-version ] ; then
+		patchver=`cat ./patch/thirdparty-patch-version`
+	else
+		patchver=""
+	fi
+
+	wget -q http://download.lxcenter.org/download/thirdparty/kloxo-version.list > /dev/null
+	kloxover=`cat kloxo-version.list`
+	rm -f kloxo-version.list
+	
+	kloxo_check_version $patchver $kloxover
+	
+	if [ "$?" -eq "1" ] ; then
+		echo "- Set ownership and permissions"
+		chown -R lxlabs:lxlabs ./patch/thirdparty/
+		find ./patch/thirdparty/ -type f -name \"*.php*\" -exec chmod 644 {} \;
+		find ./patch/thirdparty/ -type d -exec chmod 755 {} \;
+	
+		echo "- Copy patch file"
+		cp -rf ./patch/thirdparty/* /usr/local/lxlabs/kloxo/httpdocs/thirdparty
+	else
+		kloxo_exit
+	fi
+}
+
+function kloxo_webmail_portion () {
+	echo "Step for Kloxo Webmail..."
+
+	if [ -f ./patch/webmail-patch-version ] ; then
+		patchver=`cat ./patch/webmail-patch-version`
+	else
+		patchver=""
+	fi
+
+	wget -q http://download.lxcenter.org/download/version/lxwebmail
+	kloxover=`cat lxwebmail`
+	rm -f lxwebmail
+	
+	kloxo_check_version $patchver $kloxover
+	
+	if [ "$?" -eq "1" ] ; then
+		echo "- Set ownership and permissions"
+		chown -R lxlabs:lxlabs ./patch/webmail/
+		find ./patch/webmail/ -type f -name \"*.php*\" -exec chmod 644 {} \;
+		find ./patch/webmail/ -type d -exec chmod 755 {} \;
+	
+		echo "- Copy patch file"
+		cp -rf ./patch/webmail/* /home/kloxo/httpd/webmail
+	else
+		kloxo_exit
+	fi
+}
+
+function kloxo_awstats_portion () {
+	echo "Step for Kloxo Webmail..."
+
+	if [ -f ./patch/awstats-patch-version ] ; then
+		patchver=`cat ./patch/awstats-patch-version`
+	else
+		patchver=""
+	fi
+
+	wget -q http://download.lxcenter.org/download/version/lxawstats
+	kloxover=`cat lxawstats`
+	rm -f lxawstats
+	
+	kloxo_check_version $patchver $kloxover
+	
+	if [ "$?" -eq "1" ] ; then
+		echo "- Set ownership and permissions"
+		chown -R lxlabs:lxlabs ./patch/awstats/
+		find ./patch/awstats/ -type f -name \"*.php*\" -exec chmod 644 {} \;
+		find ./patch/awstats/ -type d -exec chmod 755 {} \;
+	
+		echo "- Copy patch file"
+		cp -rf ./patch/awstats/* /home/kloxo/httpd/awstats
+	else
+		kloxo_exit
+	fi
+}
+
+function kloxo_run_script () {
+	echo "Step for run script files for fixed..."
+
+	get_yes_no "    Do you want fixed?" 1
+	if [ "$?" -eq "1" ] ; then
+		sh /script/upcp
+		sh /script/cleanup
+		sh /script/fixweb
+		sh /script/fixdns
+		sh /script/fixmail
+		sh /script/fixwebmail
+	fi
+}
+
+function kloxo_begin () {
+	echo
+	echo "Begin..."
+	echo
+}
+
+function kloxo_end () {
+	echo
+	echo "... the end"
+	echo
+	exit
+}
+
+function kloxo_exit () {
+	get_yes_no "    Do you want to exit?" 1
+	if [ "$?" -eq "1" ] ; then
+		exit
+	fi
+}
+
+function kloxo_check_version () {
+
+	if [ "$patchver" ==  "$kloxover" ] ; then
+		echo "- Version '$kloxover' equal to patch version '$patchver', patch processing..."
+		return 1
+	else
+		echo "- Version '$kloxover' but patch version '$patchver', no patch process"
+		return 0
+	fi
+
+}
+
+### execution ###
+
 if [ "$#" == 0 ] ; then
 	echo
 	echo " -------------------------------------------------------------------"
@@ -30,64 +251,22 @@ if [ "$#" == 0 ] ; then
 	echo " -------------------------------------------------------------------"
 	echo "  --type - master or slave"
 	echo
+	echo " * Note: - patch dirs inside ./patch"
 	echo " * Run kloxo-packer.sh to make kloxo packs (local copy)"
 	echo
 	exit;
 fi
 
-patchver=`cat ./patch/patch-version`
-kloxover=`sh /script/version`
+kloxo_begin
 
-if [ "$patchver" ==  "$kloxover" ] ; then
-	echo "Kloxo version $kloxover equal to patch version $patchver"
-	echo
-else
-	echo "Kloxo version $kloxover but patch version $patchver"
-	echo "... the end"
-	echo
-	exit;
-fi
+kloxo_core_portion
 
-echo "Start patch..."
-echo
+kloxo_thirdparty_portion
 
-echo "- Set ownership and permissions"
-chown -R lxlabs:lxlabs ./patch/kloxo/
-find ./patch/kloxo/ -type f -name \"*.php*\" -exec chmod 644 {} \;
-find ./patch/kloxo/ -type d -exec chmod 755 {} \;
+kloxo_webmail_portion
 
-echo "- Copy patch file"
-cp -rf ./patch/kloxo/* /usr/local/lxlabs/kloxo
-cp -rf /usr/local/lxlabs/kloxo/pscript /script
-cp -rf /usr/local/lxlabs/kloxo/httpdocs/htmllib/script /script
+kloxo_awstats_portion
 
+kloxo_run_script
 
-if [ -f /usr/lib64 ] ; then
-
-	if [ ! -h /usr/lib/kloxophp ] ; then
-		echo "- /usr/lib/kloxophp not as symlink, deleted"
-		rm -rf /usr/lib/kloxophp
-	fi
-	
-	echo "- Set symlink for 64bit version"
-	mkdir -p /usr/lib64/php
-	ln -s /usr/lib64/php /usr/lib/php
-	mkdir -p /usr/lib64/httpd
-	ln -s /usr/lib64/httpd /usr/lib/httpd
-	mkdir -p /usr/lib64/lighttpd
-	ln -s /usr/lib64/lighttpd /usr/lib/lighttpd
-	mkdir -p /usr/lib64/kloxophp
-	ln -s /usr/lib64/kloxophp /usr/lib/kloxophp
-fi
-
-echo "- Run some script files for fixed"
-sh /script/upcp
-sh /script/cleanup
-sh /script/fixweb
-sh /script/fixdns
-sh /script/fixmail
-sh /script/fixwebmail
-
-echo
-echo "... the end"
-echo
+kloxo_end
