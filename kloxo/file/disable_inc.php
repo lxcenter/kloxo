60a0ffaff276d9e8034097234d222a7e8e5847eb
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-10-22 09:23:21 +0000

    issue #646 - better appear for disable page and more customize for index.php

--- kloxo/file/disable_inc.php
+++ kloxo/file/disable_inc.php
@@ -1,6 +1,6 @@
 	<br><br>
 	<div align="center">
-		<table width="100%" cellspacing="0" cellpadding="0">
+		<table width="600" cellspacing="0" cellpadding="0">
 			<tr>
 				<td><br />
 				<font size="3" face="arial" color="#336699"><strong>This is the 
@@ -12,7 +12,7 @@
 			<tr>
 				<td width="100%" bgcolor="#F5F5F5">
 				<p><font size="2" face="arial" color="#444444">If you are seeing 
-				this page, it means that web has not been DISABLED. </font></p>
+				this page, it means that web has been DISABLED. </font></p>
 				<p><font size="2" face="arial" color="#444444">Please contact the 
 				adminstrator...<br />
 				</font></p>
