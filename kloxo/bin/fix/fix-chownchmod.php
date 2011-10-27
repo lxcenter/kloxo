9a9798ab9e460edd7420e03841118a7a9e7c37fe
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-10-27 06:19:37 +0000

    Bug #705 - [Beta] - change from 770 to 750 for /home/<client> inside fix-chownchmod script

--- kloxo/bin/fix/fix-chownchmod.php
+++ kloxo/bin/fix/fix-chownchmod.php
@@ -23,16 +23,21 @@ function setFixChownChmod($select)
 	$list = $login->getList('client');
 
 	log_cleanup("Fix chown and chmod for domains");
+	
+	$userdirchmod = '750';
+	$phpfilechmod = '644';
+	$domdirchmod = '755';
 
 	foreach($list as $c) {
 		$clname = $c->getPathFromName('nname');
 		$cdir = "/home/{$clname}";
 		$dlist = $c->getList('domaina');
-
+	
 		system("chown {$clname}:apache {$cdir}/");
 		log_cleanup("- chown {$clname}:apache FOR {$cdir}/");
-		system("chmod 770 {$cdir}/");
-		log_cleanup("- chmod 770 FOR {$cdir}/");
+		
+		system("chmod {$userdirchmod} {$cdir}/");
+		log_cleanup("- chmod {$userdirchmod} FOR {$cdir}/");
 
 		foreach((array) $dlist as $l) {
 			$web = $l->nname;
@@ -41,11 +46,13 @@ function setFixChownChmod($select)
 				system("chown -R {$clname}:{$clname} {$cdir}/{$web}/");
 				log_cleanup("- chown {$clname}:{$clname} FOR {$cdir}/{$web}/ AND INSIDE");
 			}
+			
 			if (($select === "all") || ($select === 'chmod')) {
-				system("find {$cdir}/{$web}/ -type f -name \"*.php*\" -exec chmod 644 {} \;");
-				log_cleanup("- chmod 644 FOR *.php* INSIDE {$cdir}/{$web}/");
-				system("find {$cdir}/{$web}/ -type d -exec chmod 755 {} \;");
-				log_cleanup("- chmod 775 FOR {$cdir}/{$web}/ AND INSIDE");
+				system("find {$cdir}/{$web}/ -type f -name \"*.php*\" -exec chmod {$phpfilechmod} \{\} \\;");
+				log_cleanup("- chmod {$phpfilechmod} FOR *.php* INSIDE {$cdir}/{$web}/");
+				
+				system("find {$cdir}/{$web}/ -type d -exec chmod {$domdirchmod} \{\} \\;");
+				log_cleanup("- chmod {$domdirchmod} FOR {$cdir}/{$web}/ AND INSIDE");
 			}
 		}
 	}
