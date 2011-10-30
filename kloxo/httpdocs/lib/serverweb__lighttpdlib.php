5c921edd0f6934399cbea9d5c09006d24543af0d
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-10-30 00:50:46 +0000

    Issue #632 - minimize 'information box' height for 'webserver config' and fix information 'chmod 775' to 'chmod 755' and appear 'mysql-convert' and 'fix-chownchmod' on 'webserver config' under lighttpd

--- kloxo/httpdocs/lib/serverweb__lighttpdlib.php
+++ kloxo/httpdocs/lib/serverweb__lighttpdlib.php
@@ -2,5 +2,35 @@
 
 class serverweb__lighttpd extends lxDriverClass {
 
+function dbactionUpdate($subaction)
+{
+	// issue #571, #566, #567 - also mysql-convert and fix-chownchmod for lighttpd
+	
+	$m = $this->main->mysql_convert;
+	$f = $this->main->fix_chownchmod;
+
+	if ($f === 'fix-ownership') {
+		system("lphp.exe /usr/local/lxlabs/kloxo/bin/fix/fix-chownchmod.php --select=chown");
+	//	setFixChownChmod('chown');
+	}
+	else if ($f === 'fix-permissions') {
+		system("lphp.exe /usr/local/lxlabs/kloxo/bin/fix/fix-chownchmod.php --select=chmod");
+	//	setFixChownChmod('chmod');
+	}
+	else if ($f === 'fix-ALL') {
+		system("lphp.exe /usr/local/lxlabs/kloxo/bin/fix/fix-chownchmod.php --select=all");
+	//	setFixChownChmod('all');
+	}
+	
+	if ($m === 'to-myisam') {
+		system("lphp.exe /usr/local/lxlabs/kloxo/bin/fix/mysql-convert.php --engine=myisam");
+	//	setMysqlConvert('myisam');
+	}
+	else if ($m === 'to-innodb') {
+		system("lphp.exe /usr/local/lxlabs/kloxo/bin/fix/mysql-convert.php --engine=innodb");
+	//	setMysqlConvert('innodb');
+	}
+}
 
 }
+
