76f14fb2d611b7167670d160034b9638079fd24c
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-11-23 07:49:54 +0000

    [6.1.10beta] - add '--help' argument also for fixdns

--- kloxo/bin/fix/fixdns.php
+++ kloxo/bin/fix/fixdns.php
@@ -5,6 +5,9 @@ initProgram('admin');
 
 $list = parse_opt($argv);
 
+if (isset($list['server'])) { $server = $list['server']; }
+else { $server = 'localhost'; }
+
 log_cleanup("Fixing DNS server config");
 
 if (isset($list['new_dnstemplate'])) {
@@ -21,8 +24,13 @@ $list = $login->getList('client');
 
 foreach($list as $c) {
 	$dlist = $c->getList('domaina');
+	
 	foreach($dlist as $l) {
 		$dns = $l->getObject('dns');
+
+		if ($server !== 'all') {
+			if ($dns->syncserver !== $server) { continue; }
+		}
 
 		if ($dnst) {
 			$dns->dns_record_a = null;
