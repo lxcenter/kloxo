b45022b27ca342f3cd8142d2bb4b4dac4b3d30c2
shakaran <shakaran@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-11-26 03:29:13 +0000

    Security bugfix: Don't use "rand" password on lxpopuser and generate a real random salt password

--- kloxo/bin/misc/fixvpop.php
+++ kloxo/bin/misc/fixvpop.php
@@ -3,6 +3,7 @@
 include_once "htmllib/lib/include.php"; 
 
 $pass = slave_get_db_pass();
+$salt = sha1(rand());
 
-system("sh ../bin/misc/vpop.sh 'root' '$pass' lxpopuser rand"); 
+system("sh ../bin/misc/vpop.sh 'root' '$pass' lxpopuser " . $salt); 
 
