d59bd6fd503856a062b15bb653c3cb8319b48acd
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-12-10 21:06:10 +0000

    #617 - Add AWStats 7.0

---
+++ kloxo/other-thirdparty/awstats/wwwroot/cgi-bin/lib/mime.pm
@@ -0,0 +1,205 @@
+# AWSTATS MIME DATABASE
+#-------------------------------------------------------
+# If you want to add MIME types,
+# you must add an entry in MimeHashLib and assign it to a family
+#-------------------------------------------------------
+# $Revision: 1.27 $ - $Author: manolamancha $ - $Date: 2010/06/09 14:46:19 $
+
+
+#package AWSMIME;
+
+# MimeHashFamily
+# This is a hash table of mime groupings and descriptions. 
+# Report icons will appear if names the same as a family, e.g.
+# if you have a "text.png" icon in the icon/mime directory, the
+# report will load the icon
+# Format: 	'family', 'descriptive text',
+#---------------------------------------------------------------
+%MimeHashFamily = (
+'text',      'Text file',
+'page',      'HTML or XML static page',
+'script',    'Dynamic Html page or Script file',
+'pl',        'Dynamic Perl Script file',
+'php',       'Dynamic PHP Script file',
+'image',     'Image',
+'document',  'Document',
+'package',   'Package',
+'archive',   'Archive',
+'audio',     'Audio file',
+'video',     'Video file',
+'jscript',	 'JavaScript file',
+'vbs',       'Visual Basic script',
+'conf',      'Config file',
+'css',       'Cascading Style Sheet file',
+'xsl',       'Extensible Stylesheet Language file',
+'runtime',   'Binary runtime',
+'library',   'Binary library',
+'swf',       'Adobe Flash Animation',
+'flv',       'Adobe Flash Video',
+'dtd',       'Document Type Definition',
+'csv',       'Comma Separated Value file',
+'jnlp',      'Java Web Start launch file',
+'lit',       'Microsoft Reader e-book',
+'svg',       'Scalable Vector Graphics',
+'ai',        'Adobe Illustrator file',
+'phshop',    'Adobe Photoshop image file',
+'ttf',       'TrueType scalable font file',
+'fon',       'Font file',
+'pdf',       'Adobe Acrobat file',
+'dotnet',	 'Dot Net Dynamic Script or File',
+'mdb', 		 'MS Database Object',
+'crystal',	 'Crystal Reports data or file',
+'ooffice',	 'Open Office Document',
+'encrypt',	 'Encrypted document',
+);
+
+# MimeHashLib
+# This is a hash of arrays where the key is a specific file extension
+# and the array is a list of family and file type, e.g. 'd' for download
+# If a file does not have a type defined, it is counted as a page. Each
+# mime entry can have only one type
+# Format:	'extension', ['family', 'type'],
+# Valid Types:
+#		i - image
+#		d - download
+#		p - page
+#---------------------------------------------------------------
+%MimeHashLib=(
+# Text file
+'txt',['text','d'],
+'log',['text','d'],
+# HTML Static page
+'chm',['page',''],
+'html',['page',''],
+'htm',['page',''],
+'mht',['page',''],
+'wml',['page',''],
+'wmlp',['page',''],
+'xhtml',['page',''],
+'xml',['page',''],
+'vak',['page',''],
+'sgm',['page',''],
+'sgml',['page',''],
+# HTML Dynamic pages or script
+'asp',['script',''],
+'aspx',['dotnet',''],
+'ashx',['dotnet',''], 
+'asmx',['dotnet',''],
+'axd', ['dotnet',''],
+'cfm',['script',''],
+'class',['script',''],
+'js',['jscript',''],
+'jsp',['script',''],
+'cgi',['script',''],
+'ksh',['script',''],
+'php',['php',''],
+'php3',['php',''],
+'php4',['php',''],
+'pl',['pl',''],
+'py',['script',''],
+'rss',['rss',''],
+'sh',['script',''],
+'shtml',['script',''],
+'tcl',['script',''],
+'xsp',['script',''],
+'vbs',['vbs',''],
+# Image
+'gif',['image','i'],
+'png',['image','i'],
+'bmp',['image','i'],
+'jpg',['image','i'],
+'jpeg',['image','i'],
+'cdr',['image','d'],
+'ico',['image','i'],
+'svg',['image','i'],
+# Document
+'ai',['document','d'],
+'doc',['document','d'],
+'wmz',['document','d'],
+'rtf',['document','d'],
+'mso',['document','d'],
+'pdf',['pdf','d'],
+'frl',['pdf','d'],
+'xls',['document','d'],
+'ppt',['document','d'],
+'pps',['document','d'],
+'psd',['document','d'],
+'sxw',['ooffice','d'],
+'sxc',['ooffice','d'],
+'sxi',['ooffice','d'],
+'sxd',['ooffice','d'],
+'sxm',['ooffice','d'],
+'sxg',['ooffice','d'],
+'csv',['csv','d'],
+'xsl',['xsl','d'],
+'lit',['lit','d'],
+'mdb',['mdb',''],
+'rpt',['crystal',''],
+# Package
+'rpm',[($LogType eq 'S'?'audio':'package'),'d'],
+'deb',['package','d'],
+'msi',['package','d'],
+# Archive
+'7z',['archive','d'],
+'ace',['archive','d'],
+'bz2',['archive','d'],
+'cab',['archive','d'],
+'emz',['archive','d'],
+'gz',['archive','d'],
+'jar',['archive','d'],
+'lzma',['archive','d'],
+'rar',['archive','d'],
+'tar',['archive','d'],
+'tgz',['archive','d'],
+'tbz2',['archive','d'],
+'z',['archive','d'],
+'zip',['archive','d'],
+# Audio
+'aac',['audio','d'],
+'flac',['audio','d'],
+'mp3',['audio','d'],
+'oga',['audio','d'],
+'ogg',['audio','d'],
+'wav',['audio','d'],
+'wma',['audio','d'],
+'m4a',['audio','d'],
+'m3u',['audio','d'],
+'asf',['audio','d'],
+# Video
+'avi',['video','d'],
+'divx',['video','d'],
+'mp4',['video','d'],
+'m4v',['video','d'],
+'mpeg',['video','d'],
+'mpg',['video','d'],
+'ogv',['video','d'],
+'ogx',['video','d'],
+'rm',['video','d'],
+'swf',['swf',''],
+'flv',['flv','d'],
+'f4v',['flv','d'],
+'wmv',['video','d'],
+'wmf',['video','d'],
+'mov',['video','d'],
+'qt',['video','d'],
+# Config
+'cf',['conf',''],
+'conf',['conf',''],
+'css',['css',''],
+'ini',['conf',''],
+'dtd',['dtd',''],
+# Program
+'exe',['runtime',''],
+'jnlp',['jnlp',''],
+'dll',['library',''],
+'bin',['library',''],
+# Font
+'ttf',['ttf',''],
+'fon',['fon',''],
+# Encrypted files
+'pgp',['encrypt',''],
+'gpg',['encrypt',''],
+);
+
+
+1;
---
+++ kloxo/other-thirdparty/awstats/wwwroot/cgi-bin/plugins/example/example.pm
@@ -0,0 +1,327 @@
+#!/usr/bin/perl
+#-----------------------------------------------------------------------------
+# Example AWStats plugin
+# <-----
+# THIS IS A SAMPLE OF AN EMPTY PLUGIN FILE WITH INSTRUCTIONS TO HELP YOU TO
+# WRITE YOUR OWN WORKING PLUGIN. REPLACE THIS SENTENCE WITH THE PLUGIN GOAL.
+# NOTE THAT A PLUGIN FILE example.pm MUST BE IN LOWER CASE.
+# ----->
+#-----------------------------------------------------------------------------
+# Perl Required Modules: Put here list of all required plugins
+#-----------------------------------------------------------------------------
+# $Revision: 1.16 $ - $Author: eldy $ - $Date: 2008/11/15 16:03:42 $
+
+
+# <-----
+# ENTER HERE THE USE COMMAND FOR ALL REQUIRED PERL MODULES
+#if (!eval ('require "TheModule.pm";')) { return $@?"Error: $@":"Error: Need Perl module TheModule"; }
+# ----->
+#use strict;
+no strict "refs";
+
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN VARIABLES
+#-----------------------------------------------------------------------------
+# <-----
+# ENTER HERE THE MINIMUM AWSTATS VERSION REQUIRED BY YOUR PLUGIN
+# AND THE NAME OF ALL FUNCTIONS THE PLUGIN MANAGE.
+# EACH POSSIBLE FUNCTION AND GOAL ARE DESCRIBED LATER.
+my $PluginNeedAWStatsVersion="6.7";
+my $PluginHooksFunctions="xxx";
+# ----->
+
+# <-----
+# IF YOUR PLUGIN NEED GLOBAL VARIABLES, THEY MUST BE DECLARED HERE.
+use vars qw/
+$PluginVariable1
+/;
+# ----->
+
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: Init_pluginname
+#-----------------------------------------------------------------------------
+sub Init_example {
+	my $InitParams=shift;
+	my $checkversion=&Check_Plugin_Version($PluginNeedAWStatsVersion);
+
+	# <-----
+	# ENTER HERE CODE TO DO INIT PLUGIN ACTIONS
+	debug(" InitParams=$InitParams",1);
+	$PluginVariable1="";
+	# ----->
+
+	return ($checkversion?$checkversion:"$PluginHooksFunctions");
+}
+
+
+
+# HERE ARE ALL POSSIBLE HOOK FUNCTIONS. YOU MUST CHANGE THE NAME OF THE
+# FUNCTION xxx_example INTO xxx_pluginname (pluginname in lower case).
+# NOTE THAT IN PLUGINS' FUNCTIONS, YOU CAN USE ANY AWSTATS GLOBAL VARIALES.
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLStyles_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML styles at beginning of BODY section.
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLStyles_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLBodyHeader_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML code at beginning of BODY section (top of page).
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLBodyHeader_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLBodyFooter_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML code at end of BODY section (bottom of page).
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLBodyFooter_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLMenuHeader_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML code just before the menu section
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLMenuHeader_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLMenuFooter_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML code just after the menu section
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLMenuFooter_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: AddHTMLContentHeader_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to Add HTML code just before the first report
+# Parameters: None
+#-----------------------------------------------------------------------------
+sub AddHTMLContentHeader_example {
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNTION: BuildFullHTMLOutput_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to output an HTML page completely built by plugin instead
+# of AWStats output
+#-----------------------------------------------------------------------------
+sub BuildFullHTMLOutput_example {
+	# <-----
+	print "This is an output for plugin example<br />\n";
+	return 1;
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: ShowInfoHost_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to add additionnal columns to the Hosts report.
+# This function is called when building rows of the report (One call for each
+# row). So it allows you to add a column in report, for example with code :
+#   print "<TD>This is a new cell for $param</TD>";
+# Parameters: Host name or ip
+#-----------------------------------------------------------------------------
+sub ShowInfoHost_example {
+    my $param="$_[0]";
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: ShowPagesAddField_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function used to add additionnal columns to the Top Pages-URL report.
+# This function is called when building rows of the report (One call for each
+# row). So it allows you to add a column in report, for example with code :
+#   print "<TD>This is a new cell for $param</TD>";
+# Parameters: URL
+#-----------------------------------------------------------------------------
+sub ShowPagesAddField_example {
+    my $param="$_[0]";
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: ShowInfoURL_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to add additionnal information for URLs in URLs' report.
+# This function is called after writing the URL value in the URL cell of the
+# Top Pages-URL report.
+# Parameters: URL
+#-----------------------------------------------------------------------------
+sub ShowInfoURL_example {
+    my $param="$_[0]";
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: ShowInfoUser_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+# Function called to add additionnal columns to Authenticated users report.
+# This function is called when building rows of the report (One call for each
+# row). So it allows you to add a column in report, for example with code :
+#   print "<TD>This is a new cell for $param</TD>";
+# Parameters: User
+#-----------------------------------------------------------------------------
+sub ShowInfoUser_example {
+    my $param="$_[0]";
+	# <-----
+	# PERL CODE HERE
+	# ----->
+}
+
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: SectionInitHashArray_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+#-----------------------------------------------------------------------------
+sub SectionInitHashArray_example {
+	# <-----
+	if ($Debug) { debug(" Plugin example: Init_HashArray"); }
+	%_myarray_p = %_myarray_h = %_myarray_k = %_myarray_l = ();
+	# ----->
+	return 0;
+}
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: SectionProcessIP_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+#-----------------------------------------------------------------------------
+sub SectionProcessIp_example {
+    my $param="$_[0]";      # Param is IP of record
+	# <-----
+	# PERL CODE HERE
+	# ----->
+	return;
+}
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: SectionProcessHostname_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+#-----------------------------------------------------------------------------
+sub SectionProcessHostname_example {
+    my $param="$_[0]";      # Param is hostname of record
+	# <-----
+	# PERL CODE HERE
+	# ----->
+	return;
+}
+
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: SectionReadHistory_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+#-----------------------------------------------------------------------------
+sub SectionReadHistory_example {
+    my $issectiontoload=shift;
+    my $xmlold=shift;
+    my $xmleb=shift;
+	my $countlines=shift;
+	# <-----
+#	if ($Debug) { debug(" Plugin example: Begin of PLUGIN_example section"); }
+#	my @field=();
+#	my $count=0;my $countloaded=0;
+#	do {
+#		if ($field[0]) {
+#			$count++;
+#			if ($issectiontoload) {
+#				$countloaded++;
+#				if ($field[1]) { $_myarray_p{$field[0]}+=$field[1]; }
+#				if ($field[2]) { $_myarray_h{$field[0]}+=$field[2]; }
+#				if ($field[3]) { $_myarray_k{$field[0]}+=$field[3]; }
+#				if ($field[4]) { $_myarray_l{$field[0]}+=$field[4]; }
+#			}
+#		}
+#		$_=<HISTORY>;
+#		chomp $_; s/\r//;
+#		@field=split(/\s+/,($xmlold?XMLDecodeFromHisto($_):$_));
+#		$countlines++;
+#	}
+#	until ($field[0] eq 'END_PLUGIN_example' || $field[0] eq "${xmleb}END_PLUGIN_example" || ! $_);
+#	if ($field[0] ne 'END_PLUGIN_example' && $field[0] ne "${xmleb}END_PLUGIN_example") { error("History file is corrupted (End of section PLUGIN not found).\nRestore a recent backup of this file (data for this month will be restored to backup date), remove it (data for month will be lost), or remove the corrupted section in file (data for at least this section will be lost).","","",1); }
+#	if ($Debug) { debug(" Plugin example: End of PLUGIN_example section ($count entries, $countloaded loaded)"); }
+	# ----->
+	return 0;
+}
+
+#-----------------------------------------------------------------------------
+# PLUGIN FUNCTION: SectionWriteHistory_pluginname
+# UNIQUE: NO (Several plugins using this function can be loaded)
+#-----------------------------------------------------------------------------
+sub SectionWriteHistory_example {
+    my ($xml,$xmlbb,$xmlbs,$xmlbe,$xmlrb,$xmlrs,$xmlre,$xmleb,$xmlee)=(shift,shift,shift,shift,shift,shift,shift,shift,shift);
+    if ($Debug) { debug(" Plugin example: SectionWriteHistory_example start - ".(scalar keys %_myarray_h)); }
+	# <-----
+#	print HISTORYTMP "\n";
+#	if ($xml) { print HISTORYTMP "<section id='plugin_example'><sortfor>$MAXNBOFSECTIONGIR</sortfor><comment>\n"; }
+#	print HISTORYTMP "# Plugin key - Pages - Hits - Bandwidth - Last access\n";
+#	$ValueInFile{'plugin_example'}=tell HISTORYTMP;
+#	print HISTORYTMP "${xmlbb}BEGIN_PLUGIN_example${xmlbs}".(scalar keys %_myarray_h)."${xmlbe}\n";
+#	&BuildKeyList($MAXNBOFSECTIONGIR,1,\%_myarray_h,\%_myarray_h);
+#	my %keysinkeylist=();
+#	foreach (@keylist) {
+#		$keysinkeylist{$_}=1;
+#		my $page=$_myarray_p{$_}||0;
+#		my $bytes=$_myarray_k{$_}||0;
+#		my $lastaccess=$_myarray_l{$_}||'';
+#		print HISTORYTMP "${xmlrb}$_${xmlrs}", $_myarray_p{$_}, "${xmlrs}", $_myarray_h{$_}, "${xmlrs}", $_myarray_k{$_}, "${xmlrs}", $_myarray_l{$_}, "${xmlre}\n"; next;
+#	}
+#	foreach (keys %_myarray_h) {
+#		if ($keysinkeylist{$_}) { next; }
+#		my $page=$_myarray_p{$_}||0;
+#		my $bytes=$_myarray_k{$_}||0;
+#		my $lastaccess=$_myarray_l{$_}||'';
+#		print HISTORYTMP "${xmlrb}$_${xmlrs}", $_myarray_p{$_}, "${xmlrs}", $_myarray_h{$_}, "${xmlrs}", $_myarray_k{$_}, "${xmlrs}", $_myarray_l{$_}, "${xmlre}\n"; next;
+#	}
+#	print HISTORYTMP "${xmleb}END_PLUGIN_example${xmlee}\n";
+	# ----->
+	return 0;
+}
+
+
+1;	# Do not remove this line
