e9f075e936e815d3b25eebee6fd11fa3c76d0b54
dterweij <dterweij@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-12-12 02:34:56 +0000

    #574 - Distribution tools: Change to use CKEditor.

--- kloxo/make-distribution.sh
+++ kloxo/make-distribution.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+#    Kloxo, Hosting Control Panel
+#
+#    Copyright (C) 2000-2009	LxLabs
+#    Copyright (C) 2009-2011	LxCenter
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # This file creates kloxo-[version].zip for distribution from SVN.
 # This file creates kloxo-[version].tar.gz for distribution from SVN.
@@ -11,6 +28,7 @@
 #
 # Changelog
 # may.30.2011 - Added tar.gz creation for better compression. .zip can be removed if kloxo installer/source is modified aswell.
+# dec.10.2011 - Change fckeditor to ckeditor
 #
 ######
 echo "################################"
@@ -44,7 +62,7 @@ echo "### Create zip package..."
 	"*/.svn/*" \
 	"*httpdocs/thirdparty/*" \
 	"*httpdocs/htmllib/extjs/*" \
-	"*httpdocs/htmllib/fckeditor/*" \
+	"*httpdocs/htmllib/ckeditor/*" \
 	"*httpdocs/htmllib/yui-dragdrop/*"
 
 echo "### Created ZIP package"
@@ -58,7 +76,7 @@ echo "### Create TAR.GZ package"
 	--exclude=".php.err" \
 	--exclude="thirdparty" \
 	--exclude="extjs" \
-	--exclude="fckeditor" \
+	--exclude="ckeditor" \
 	--exclude="yui-dragdrop" \
         --exclude="CVS" \
         --exclude=".svn"
