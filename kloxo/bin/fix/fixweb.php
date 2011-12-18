6dc21770d9d015a2b6be6ffae1d1ac90f500dc07
mustafaramadhan <mustafaramadhan@3beec2c5-04e5-4a48-9ea8-1a8e397cc1b9>
2011-12-18 16:56:27 +0000

    issue #824 - Optimize code for apache/lighttpd to make faster delete/add/update/fix configs; file fixweb/fixweb.php also related to #826

--- kloxo/bin/fix/fixweb.php
+++ kloxo/bin/fix/fixweb.php
@@ -5,22 +5,41 @@ initProgram('admin');
 
 $list = parse_opt($argv);
 
-if (isset($list['server'])) { $server = $list['server']; }
-else { $server = 'localhost'; }
+$server = (isset($list['server'])) ? $list['server'] : 'localhost';
+$client = (isset($list['client'])) ? $list['client'] : null;
 
 $login->loadAllObjects('client');
 $list = $login->getList('client');
 
 log_cleanup("Fixing Web server config");
 
+$prevsyncserver = '';
+$currsyncserver = '';
+
 foreach($list as $c) {
+
+	if ($client) {
+		$server = 'all';
+		if ($client !== $c->nname) { continue; }
+	}
+
+	if ($server !== 'all') {
+		if ($c->syncserver !== $server) { continue; }
+	}
+
 	$dlist = $c->getList('domaina');
 
 	foreach((array) $dlist as $l) {
 		$web = $l->getObject('web');
 
-		if ($server !== 'all') {
-			if ($web->syncserver !== $server) { continue; }
+		$currsyncserver = $web->syncserver;
+
+		if ($prevsyncserver !== $currsyncserver) {
+			$web->setUpdateSubaction('static_config_update');
+
+			log_cleanup("- inside static (defaults/webmail) directory at '{$currsyncserver}'");
+
+			$prevsyncserver = $currsyncserver;
 		}
 
 		$web->setUpdateSubaction('full_update');
