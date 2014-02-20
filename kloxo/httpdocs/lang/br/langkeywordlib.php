e6d7b56e8c415a3a9884cb06a7ed0dedc8df1182
Danny Terweij <d.terweij@lxcenter.org>
2014-02-21 00:34:11 +0100

    Add missing languages. - BR - ES - FA - NL - PL - RO - RU

---
+++ kloxo/httpdocs/lang/br/langkeywordlib.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ *    HyperVM, Server Virtualization GUI for OpenVZ and Xen
+ *
+ *    Copyright (C) 2000-2009    LxLabs
+ *    Copyright (C) 2009-2012    LxCenter
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$__keyword['under'] = 'Under';
+$__keyword['filtered'] = 'Filtered';
+$__keyword['no'] = 'No';
+$__keyword['matching_the_filter'] = 'Matching the Filter';
+$__keyword['no_matches_found'] = 'No Matches Found';
+$__keyword['db_will_be_used'] = 'New Database will be created';
+$__keyword['master_cannot_be_assigned'] = 'Master cannot be assigned to a client';
+$__keyword['resource'] = 'Resources';
+$__keyword['help'] = 'Help';
+$__keyword['desktop'] = 'Desktop';
+$__keyword['home'] = 'Home';
+$__keyword['manage'] = 'Manage';
+$__keyword['list'] = 'List';
+$__keyword['extra'] = 'Extra';
+$__keyword['system'] = 'System';
+$__keyword['administration'] = 'Administration';
+$__keyword['logout'] = 'Logout';
+$__keyword['about'] = 'About';
+$__keyword['status'] = 'Status';
+$__keyword['enable'] = 'Enable';
+$__keyword['disable'] = 'Disable';
+$__keyword['console'] = 'Console';
+$__keyword['webmailanddb'] = 'Web - Mail - Database';
+$__keyword['trafficandlog'] = 'Traffic - Logs';
+$__keyword['script'] = 'script';
+$__keyword['action'] = 'Action';
+$__keyword['power'] = 'Power';
+$__keyword['cpenabled'] = 'CP Enabled';
+$__keyword['cpdisabled'] = 'CP Disabled';
+$__keyword['virtual'] = 'virtual';
+$__keyword['continue_without_plan'] = 'Continue Without Plan';
+$__keyword['add'] = 'Add';
+$__keyword['continue'] = 'Continue';
+$__keyword['all'] = 'All';
+$__keyword['search_on'] = 'Search On';
+$__keyword['search_note'] = 'Search is on, and thats why this list is empty. To see all results, click on show all';
+$__keyword['deprecated'] = 'Deprecated in Reseller and Admin.';
+$__keyword['console_message'] = "You are actually logging into a user (%username%) on the HOST machine, which will automatically drop you into the vps. The password is your Control Panel password for the vps. You can also login to this user %username%  using your favorite ssh client, and there too on a successful login, you will be dropped into the vps. Note that you don't need an ipaddress to be configured on the vps to use this facility. You are basically connecting to the HOST as user, which will automatically transfer you to the vps.";
+$__keyword['advanced'] = 'Advanced';
+$__keyword['domain'] = 'Domain';
