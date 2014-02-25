ae0c4dd7ab8c6cfd8746943d0469458dd6f27ad9
Semirke <support@profivps.hu>
2014-02-25 03:18:27 +0100

    Hungarian language patch v0.1
      Not full, many info and message is not translated. If anyone willing, feel free to send me the translated files.

--- kloxo/httpdocs/htmllib/phplib/lxlib.php
+++ kloxo/httpdocs/htmllib/phplib/lxlib.php
@@ -1880,6 +1880,17 @@ function get_language()
 
 function get_charset()
 {
+  $lang = get_language();
+  $charset = @ lfile_get_contents("lang/$lang/charset");
+  $charset = trim($charset);
+  if(!$charset || $charset=="") $charset="UTF-8";
+  return $charset;
+}
+                                
+                                
+
+function get_charset()
+{
 	$lang = get_language();
 	$charset = @ lfile_get_contents("lang/$lang/charset");
 	$charset = trim($charset);
