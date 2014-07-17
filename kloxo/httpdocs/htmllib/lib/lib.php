4cf7eb3c1d256120923fd7f4536f2b1b4f295e26
Semirke <support@profivps.hu>
2014-07-17 22:09:14 +0200

    FIX: invalid_email_id
    Because of the long domain LTD, user cannot add email.

--- kloxo/httpdocs/htmllib/lib/lib.php
+++ kloxo/httpdocs/htmllib/lib/lib.php
@@ -1331,7 +1331,7 @@ function do_actionlog($login, $object, $action, $subaction)
 function validate_email($email)
 {
 	$regexp = "/^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@" .
-		"((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i";
+		"((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,9})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i";
 	if(!preg_match($regexp, $email)) {
 		return false;
 	}
